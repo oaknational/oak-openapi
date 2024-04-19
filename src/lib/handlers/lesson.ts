9aab68c5261758f8249696957d92152a03e29cd2
Remy Sharp <remy@remysharp.com>
2024-04-19 18:22:52 +0100

    feat: lessons search

--- src/lib/handlers/lesson.ts
+++ src/lib/handlers/lesson.ts
@@ -1,7 +1,14 @@
 import { protectedProcedure } from '~/lib/auth';
 import { router } from '~/lib/trpc';
 import { TRPCError } from '@trpc/server';
-import { LessonView, getClient, gql, lessonView } from 'lib/owaClient';
+import {
+  LessonView,
+  getClient,
+  gql,
+  lessonView,
+  lessonViewTable,
+  querySQL,
+} from 'lib/owaClient';
 import { z } from 'zod';
 
 const lessonSummary = z.object({
@@ -43,6 +50,11 @@ export const getLessons = router({
         tags: ['lessons'],
         path: '/lessons/{lesson}/summary',
         description: 'Get a summary of the specified lesson',
+        example: {
+          request: {
+            lesson: 'simple-compound-and-adverbial-complex-sentences',
+          },
+        },
       },
     })
     .input(
@@ -95,5 +107,88 @@ export const getLessons = router({
 
       const lesson = data[0] as LessonSummary;
       return lesson;
+    }),
+  searchByTextSimilarity: protectedProcedure
+    .meta({
+      // FIXME what is this exactly?
+      openapi: {
+        method: 'GET',
+        tags: ['lessons', 'search'],
+        path: '/search/lessons/text-similarity',
+        description: 'Find lessons with a similar title as the given text',
+      },
+    })
+    .input(
+      z.object({
+        q: z.string(),
+      })
+    )
+    .output(
+      z.any()
+      // z.array(
+      //   z.object({
+      //     slug: z.string(),
+      //     title: z.string(),
+      //     description: z.string(),
+      //   })
+      // )
+    )
+    .query(async ({ ctx, input }) => {
+      // store q from input.q and sanitize for use as an sql query:
+      const q = input.q.replace(/'/g, "''");
+
+      const result = await querySQL(
+        `SELECT * from (SELECT "lessonSlug", SIMILARITY("lessonTitle", '${q}') FROM ${lessonViewTable}) as a order by a.similarity desc limit 20`
+      ).then((res) => res.json());
+
+      const slugs = result.result.slice(1).map(([slug]: [string]) => slug);
+
+      const client = getClient();
+
+      const query = gql`
+        query ($slugs: [String!]!) {
+          ${lessonView}(where: { lessonSlug: { _in: $slugs } }) {
+            lessonSlug
+            lessonTitle
+          }
+        }
+      `;
+
+      const res = await client.request(query, { slugs });
+
+      if (res[lessonView].length === 0) {
+        throw new TRPCError({
+          message: 'No lessons found',
+          code: 'NOT_FOUND',
+        });
+      }
+
+      return res[lessonView];
+
+      // const res = await ctx.prisma.lesson.findMany({
+      //   select: {
+      //     title: true,
+      //     slug: true,
+      //     content: true,
+      //   },
+      //   where: { id: { in: result.map((r) => r.id) } },
+      // });
+
+      // if (!res) {
+      //   throw new TRPCError({
+      //     message: 'No lessons found',
+      //     code: 'NOT_FOUND',
+      //   });
+      // }
+
+      // return res.map(({ slug, title, content }) => {
+      //   content = content as Content;
+      //   const description = (content?.lessonDescription || '') as string;
+      //   return {
+      //     slug,
+      //     title,
+      //     description,
+      //   };
+      // });
     }),
 });
