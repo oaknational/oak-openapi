8e608561b5bc9c4a271d753efc75283a5781360a
Remy Sharp <remy@remysharp.com>
2024-04-25 16:43:33 +0100

    feat: pagination on assets

--- src/lib/handlers/assets.ts
+++ src/lib/handlers/assets.ts
@@ -4,16 +4,13 @@ import { z } from 'zod';
 
 import { protectedProcedure } from '~/lib/auth';
 import { router } from '../trpc';
-import { getClient } from '../owaClient';
+import { Lesson, LessonView, getClient, lessonView } from '../owaClient';
 import { keyStageSlugs, subjectSlugs } from '../keyStageAndSubjects';
 import { baseUrl } from '../baseUrl';
+import { off } from 'process';
 
 // note: I've put these two together in the code because they're
 // directly linked
-const view = 'published_mv_lesson_openapi_1_0_0';
-export type VideoObjectView = {
-  published_mv_lesson_openapi_1_0_0: VideoObjects[];
-};
 
 // FIXME stop using this MV, need to move to my own
 const downloadView = 'published_mv_downloads_5_0_2';
@@ -28,71 +25,7 @@ export interface DownloadObjects {
   downloads: Download[];
 }
 
-export type VideoObjects = {
-  lessonSlug: string;
-  video_object: VideoObject;
-};
-
-export interface VideoObject {
-  id: string;
-  status: string;
-  tracks: Track[];
-  duration: number;
-  created_at: string;
-  mp4_support: string;
-  passthrough: string;
-  aspect_ratio: string;
-  mux_asset_id: string;
-  playback_ids: PlaybackId[];
-  encoding_tier: string;
-  master_access: string;
-  mux_playback_id: string;
-  resolution_tier: string;
-  signed_stream_id: string;
-  static_renditions: StaticRenditions;
-  max_resolution_tier: string;
-  max_stored_frame_rate: number;
-  max_stored_resolution: string;
-}
-
-export interface Track {
-  id: string;
-  type: string;
-  duration?: number;
-  max_width?: number;
-  max_height?: number;
-  max_frame_rate?: number;
-  max_channels?: number;
-  max_channel_layout?: string;
-  name?: string;
-  status?: string;
-  text_type?: string;
-  passthrough?: string;
-  text_source?: string;
-  language_code?: string;
-  closed_captions?: boolean;
-}
-
-export interface PlaybackId {
-  id: string;
-  policy: string;
-}
-
-export interface StaticRenditions {
-  files: File[];
-  status: string;
-}
-
-export interface File {
-  ext: string;
-  name: string;
-  width: number;
-  height: number;
-  bitrate: number;
-  filesize: string;
-}
-
-export type Download = {
+type Download = {
   ext: string;
   label: string;
   type: string;
@@ -141,7 +74,7 @@ const graphqlClient = getClient();
 function assetDownloadWithVideos(
   lessonSlug: string,
   downloads: Download[],
-  videoObjects: VideoObjects[]
+  videoObjects: Lesson[]
 ) {
   const videos = videoObjects.find((_) => _.lessonSlug === lessonSlug);
 
@@ -261,6 +194,14 @@ export const getAssets = router({
             description: 'Optional unit slug to additionally filter by.',
           })
           .optional(),
+        offset: z.number().optional().default(0),
+        limit: z
+          .number({
+            description: 'Limit the number of results returned, max 100',
+          })
+          .lte(100)
+          .optional()
+          .default(10),
       })
     )
     .output(
@@ -274,28 +215,37 @@ export const getAssets = router({
         })
       )
     )
-    .query(async ({ input }) => {
-      // FIXME: move to slugs instead of titles
-      const key = decodeURIComponent(input.keyStage);
-      const subject = decodeURIComponent(input.subject);
+    .query(async ({ input, ctx }) => {
+      const keyStage = input.keyStage;
+      const subject = input.subject;
       const unit = input.unit || null;
       const typeFilter = input.type || null;
 
-      // BIG FIXME - ordinarily I'd use the slugs as the joining data point,
-      // except that "our" database (the ai-beta one) has key stage slugs as
-      // "key-stage-3" which, compared to the OWA database, doesn't match
-      // and needs to be "ks3". So instead, I'm using the title, which feels
-      // brittle, but does work.
+      const offset = input.offset;
+      const limit = input.limit;
+
+      let where =
+        'keyStageSlug: { _eq: $keyStage } subjectSlug: { _eq: $subject } ';
+      if (unit) where += `unitSlug: { _eq: $unit }`;
 
       const queryVideos = gql`
         query GetAssets(
           $keyStage: String!
           $subject: String!
+          $offset: Int!
+          $limit: Int!
+          $unit: String
         ) {
-          ${view}(
-            where: { keyStageSlug: { _eq: $keyStage }, subjectSlug: { _eq: $subject } }
+          ${lessonView}(
+            where: {
+              ${where}
+            }
+            offset: $offset,
+            limit: $limit,
+            order_by: {lessonSlug: asc}
           ) {
             lessonSlug
+            lessonTitle
             unitSlug
             video_object
           }
@@ -303,12 +253,20 @@ export const getAssets = router({
       `;
 
       const queryDownloads = gql`
-        query GetDownloads($keyStage: String!, $subject: String!) {
+        query GetDownloads(
+          $keyStage: String!
+          $subject: String!
+          $offset: Int!
+          $limit: Int!
+          $unit: String
+        ) {
           ${downloadView}(
             where: {
-              keyStageSlug: { _eq: $keyStage }
-              subjectSlug: { _eq: $subject }
+              ${where}
             }
+            offset: $offset,
+            limit: $limit,
+            order_by: {lessonSlug: asc}
           ) {
             lessonSlug
             lessonTitle
@@ -319,41 +277,58 @@ export const getAssets = router({
       `;
 
       const variables = {
-        keyStage: key,
+        keyStage,
         subject,
+        unit,
+        offset,
+        limit,
+      } as {
+        offset: number;
+        limit: number;
+        keyStage: string;
+        subject: string;
+        unit?: string;
       };
 
-      const downloadsQuery: DownloadView = await graphqlClient.request(
-        queryDownloads,
-        variables
-      );
+      if (!unit) {
+        delete variables.unit;
+      }
+
+      const [downloadsQuery, videoObjectQuery] = await Promise.all([
+        graphqlClient.request(
+          queryDownloads,
+          variables
+        ) as Promise<DownloadView>,
+        graphqlClient.request(queryVideos, variables) as Promise<LessonView>,
+      ]);
 
       const res = downloadsQuery[downloadView];
 
-      const videoObjectQuery: VideoObjectView = await graphqlClient.request(
-        queryVideos,
-        variables
-      );
+      let next = null;
+      if (res.length === limit) {
+        next = `${baseUrl}${ctx.req.url}?offset=${
+          offset + limit
+        }&limit=${limit}`;
+        if (unit) {
+          next += `&unit=${unit}`;
+        }
+        ctx.res.setHeader('link', `<${next}>; rel="next"`);
+      }
 
-      return res
-        .filter((res) => {
-          if (!unit) return true;
-          return res.unitSlug === unit;
-        })
-        .map(({ downloads, lessonSlug, lessonTitle }) => {
-          return {
+      return res.map(({ downloads, lessonSlug, lessonTitle }) => {
+        return {
+          lessonSlug,
+          lessonTitle,
+          assets: assetDownloadWithVideos(
             lessonSlug,
-            lessonTitle,
-            assets: assetDownloadWithVideos(
-              lessonSlug,
-              downloads,
-              videoObjectQuery[view]
-            ).filter((asset) => {
-              if (!typeFilter) return true;
-              return asset.type === typeFilter;
-            }),
-          };
-        });
+            downloads,
+            videoObjectQuery[lessonView]
+          ).filter((asset) => {
+            if (!typeFilter) return true;
+            return asset.type === typeFilter;
+          }),
+        };
+      });
     }),
   getLessonAssets: protectedProcedure
     .meta({
@@ -450,7 +425,7 @@ export const getAssets = router({
       // also get the video assets
       const query = gql`
         query GetAssets($lessonSlug: String!) {
-          ${view}(
+          ${lessonView}(
             where: { lessonSlug: { _eq: $lessonSlug } }
           ) {
             lessonSlug
@@ -477,7 +452,7 @@ export const getAssets = router({
         });
       }
 
-      const videoObjectQuery: VideoObjectView = await graphqlClient.request(
+      const videoObjectQuery: LessonView = await graphqlClient.request(
         query,
         variables
       );
@@ -487,7 +462,7 @@ export const getAssets = router({
       return assetDownloadWithVideos(
         lessonSlug,
         downloads,
-        videoObjectQuery[view]
+        videoObjectQuery[lessonView]
       );
     }),
 });
