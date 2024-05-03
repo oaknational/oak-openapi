d54ef36ba69f14d4e2fdc547a2318f14900f2167
Remy Sharp <remy@remysharp.com>
2024-05-03 13:16:34 +0100

    feat: move keys to upstash

--- src/lib/apikeys.ts
+++ src/lib/apikeys.ts
@@ -1,12 +1,11 @@
-export const keys = [
-  '8d8bcb57-06e7-4d12-9c1d-aab62ad08d84', // oak testing
-  '07c5d40c-4721-4347-ae28-ae998385feab', // TMAI
-  '2cd6ffc0-52f6-f611-dd88-af0a49aee1f6', // Quizalize
-  '3bfb828f-cc91-f041-6428-ad3e5adb2815', // Propello
-  '7581b6a5-591f-5f72-0bdf-79e1a4274bb6', // Carousel
-  '0296d0f7-25c7-7429-d989-99099276981f', // N10
-  '230044b4-c033-2e55-6e0c-6c43335960ef', // boclips
-  'ab703b06-d45f-00a5-aaa3-21644f416602', // futurelearn
-  '681c318a-53a4-ec6e-8b61-ff762902478c', // partridge.works
-  '47c7137c-4801-21a5-9422-0843033d38d2', // matt gregory
-];
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  // @ts-ignore: url prop doesn't seem to be in the types, but it's definitely there
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export const keys = await redis.json.get('apikeys').catch((e) => {
+  console.log(`Failed to collect apikeys: ${e.toString()}`);
+});
