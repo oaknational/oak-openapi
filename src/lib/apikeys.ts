2670a4447739fe70afd364bfefd56d6cba2b5779
Remy Sharp <remy@remysharp.com>
2024-05-03 13:16:34 +0100

    feat: move keys to upstash

--- src/lib/apikeys.ts
+++ src/lib/apikeys.ts
@@ -1,12 +1,11 @@
-export const keys = [
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-  // REDACTED
-];
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  // @ts-ignore: url prop doesn't seem to be in the types, but it's definitely there
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export const keys = await redis.json.get('apikeys').catch((e) => {
+  console.log(`Failed to collect apikeys: ${e.toString()}`);
+});
