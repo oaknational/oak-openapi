3de86455c8a88c5ebf40d122c0600e7ac610728f
Remy Sharp <remy@remysharp.com>
2024-06-04 12:49:31 +0100

    feat: move to prisma accelerate for transcript search

--- src/lib/handlers/transcripts.ts
+++ src/lib/handlers/transcripts.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { aiPool } from '~/lib/db';
+import { prisma } from '~/lib/db';
 import { protectedProcedure } from '~/lib/auth';
 import { router } from '~/lib/trpc';
 
@@ -18,26 +18,26 @@ export const getTranscripts = router({
           },
           response: [
             {
-              lessonTitle: 'What is the history of London?',
-              lessonSlug: 'what-is-the-history-of-london-6dgp8r',
-            },
-            {
               lessonTitle: 'The Roman invasion of Britain ',
               lessonSlug: 'the-roman-invasion-of-britain',
+              transcriptSnippet: 'The Romans were ready,',
             },
             {
               lessonTitle:
                 'The changes to life brought about by Roman settlement',
               lessonSlug:
                 'the-changes-to-life-brought-about-by-roman-settlement',
+              transcriptSnippet: 'when the Romans came.',
             },
             {
-              lessonTitle: 'The buildings of Roman Britain ',
-              lessonSlug: 'the-buildings-of-roman-britain',
+              lessonTitle: "Boudica's rebellion against Roman rule",
+              lessonSlug: 'boudicas-rebellion-against-roman-rule',
+              transcriptSnippet: 'kings who resisted the Romans were,',
             },
             {
               lessonTitle: 'How far religion changed under Roman rule',
               lessonSlug: 'how-far-religion-changed-under-roman-rule',
+              transcriptSnippet: 'for the Romans.',
             },
           ],
         },
@@ -46,37 +46,64 @@ export const getTranscripts = router({
     .input(
       z.object({
         q: z.string({
-          description: 'The lesson slug',
+          description:
+            'A snippet of text to search for in the lesson video transcripts',
         }),
       })
     )
     .output(
-      z.array(z.object({ lessonTitle: z.string(), lessonSlug: z.string() }))
+      z.array(
+        z.object({
+          lessonTitle: z.string(),
+          lessonSlug: z.string(),
+          transcriptSnippet: z.string().optional(),
+        })
+      )
     )
     .query(async ({ input }) => {
       const { q } = input;
 
-      console.log('starting search for:', q);
-      console.time('snippet search');
+      type SearchRow = {
+        lesson_id: string;
+        source_content: string;
+      };
 
-      const search = await aiPool.query(
-        `SELECT lesson_id FROM snippets WHERE to_tsvector('english', source_content) @@ to_tsquery($1) ORDER BY similarity(source_content, $2) DESC limit 5`,
-        [q.split(' ').join(' & '), q]
-      );
-      console.timeEnd('snippet search');
+      // console.time('snippet search');
 
-      const ids = search.rows.map((r) => r.lesson_id);
+      const search: SearchRow[] =
+        await prisma.$queryRaw`SELECT lesson_id, source_content FROM snippets WHERE to_tsvector('english', source_content) @@ to_tsquery(${q
+          .split(' ')
+          .join(
+            ' & '
+          )}) ORDER BY similarity(source_content, ${q}) DESC limit 5`;
 
-      console.log('snippet complete: ', ids.length, 'results');
+      // console.timeEnd('snippet search');
 
-      const res = await aiPool.query(
-        `SELECT title as "lessonTitle", slug as "lessonSlug" FROM lessons WHERE id = ANY($1)`,
-        [ids]
-      );
+      const ids = search.map((r) => r.lesson_id);
+
+      const res = await prisma.lesson.findMany({
+        where: {
+          id: {
+            in: ids,
+          },
+        },
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+        },
+      });
 
       // sort the rows by the order of the ids array
-      return res.rows.sort((a, b) => {
-        return ids.indexOf(a.lessonSlug) - ids.indexOf(b.lessonSlug);
-      });
+      return res
+        .map((r) => ({
+          lessonTitle: r.title,
+          lessonSlug: r.slug,
+          transcriptSnippet: search.find((s) => s.lesson_id === r.id)
+            ?.source_content,
+        }))
+        .sort((a, b) => {
+          return ids.indexOf(a.lessonSlug) - ids.indexOf(b.lessonSlug);
+        });
     }),
 });
