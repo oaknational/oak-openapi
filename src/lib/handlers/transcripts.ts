86c96f1db2e831d6431da554bfc289d925cb11d8
Remy Sharp <remy@remysharp.com>
2024-06-04 10:53:36 +0100

    chore: remove unused deps

--- src/lib/handlers/transcripts.ts
+++ src/lib/handlers/transcripts.ts
@@ -1,6 +1,4 @@
 import { z } from 'zod';
-import { PGVectorStore } from 'langchain/vectorstores/pgvector';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { aiPool } from '~/lib/db';
 import { protectedProcedure } from '~/lib/auth';
 import { router } from '~/lib/trpc';
@@ -58,46 +56,11 @@ export const getTranscripts = router({
     .query(async ({ input }) => {
       const { q } = input;
 
-      // TODO: use langchain with the pgvector retriever to search using the
-      // openai embeddings model to find similar text to the `q`. This should
-      // give us back a list of lesson_ids which we can then use to query the
-      // lessons table to get the lessons.
-
-      // console.time('OpenAIEmbeddings');
-      // const embeddings = new OpenAIEmbeddings({
-      //   apiKey: process.env.OPENAI_API_KEY,
-      // });
-      // console.timeEnd('OpenAIEmbeddings');
-
-      // console.time('new PGVectorStore');
-      // const vectorStore = new PGVectorStore(embeddings, {
-      //   pool: aiPool,
-      //   tableName: 'snippets',
-      //   columns: {
-      //     vectorColumnName: 'embedding',
-      //     contentColumnName: 'source_content',
-      //     metadataColumnName: 'lesson_id',
-      //   },
-      // });
-      // console.timeEnd('new PGVectorStore');
-
-      // console.time('embedQuery');
-      // const e = await embeddings.embedQuery(q);
-      // console.log('embeddings', e);
-      // console.timeEnd('embedQuery');
-
-      // console.time('similaritySearch');
-      // const result = await vectorStore.similaritySearch(q, 5);
-      // console.timeEnd('similaritySearch');
-
-      // const ids = result.map((r) => r.metadata);
-
       const search = await aiPool.query(
         `SELECT lesson_id FROM snippets WHERE to_tsvector('english', source_content) @@ to_tsquery($1) ORDER BY similarity(source_content, $2) DESC limit 5`,
         [q.split(' ').join(' & '), q]
       );
 
-      console.log('search', search.rows);
       const ids = search.rows.map((r) => r.lesson_id);
 
       const res = await aiPool.query(
