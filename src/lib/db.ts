feb431d7a03db4fa922bacb79a0877e68bb893d7
Remy Sharp <remy@remysharp.com>
2024-06-04 12:49:31 +0100

    feat: move to prisma accelerate for transcript search

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,26 +1,4 @@
-import { Pool } from 'pg';
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-type Options = {
-  connectionString?: string;
-  ssl?: {
-    rejectUnauthorized: boolean;
-    ca: string;
-  };
-};
-
-const options: Options = {
-  connectionString: process.env.AI_DATABASE_URL?.split('?')[0],
-};
-
-if (process.env.AI_DATABASE_URL?.includes('?')) {
-  options.ssl = {
-    rejectUnauthorized: false,
-    ca: cert(), // RADAR unsure why this works when the cert is an empty string
-  };
-}
-
-export const aiPool: Pool = new Pool(options);
-
-function cert() {
-  return process.env.AI_DATABASE_CERT as string;
-}
+export const prisma = new PrismaClient().$extends(withAccelerate());
