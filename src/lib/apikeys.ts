afc31170ab6dd859021b5e90563c94c79d936e7d
Remy Sharp <remy@remysharp.com>
2024-06-06 15:50:06 +0100

    chore: refactor auth to allow for name + id to be lined to api key

--- src/lib/apikeys.ts
+++ src/lib/apikeys.ts
@@ -5,12 +5,20 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-type Keys = string[];
+export type User = { key: string; name: string; id: string };
 
-export const keys: Keys = await redis.json
+export const keys: User[] = await redis.json
   .get('apikeys')
   .catch((e: Error) => {
     console.log(`Failed to collect apikeys: ${e.toString()}`);
-    return []; // Add type assertion here
+    return [];
   })
-  .then((keys) => keys as Keys);
+  .then((keys) => keys as User[]);
+
+export async function findUserByKey(key: string): Promise<User | null> {
+  const res = await redis.json.get('apikeys', `$[?(@.key=="${key}")]`);
+  if (res) {
+    return res as User;
+  }
+  return null;
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,15 @@
+import { TRPCError } from '@trpc/server';
 import { t } from '~/lib/trpc';
 
-export const protectedProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
+  const { user } = ctx;
+
+  if (!user) {
+    throw new TRPCError({
+      message: 'API token not provided or invalid',
+      code: 'UNAUTHORIZED',
+    });
+  }
+
+  return next();
+});
