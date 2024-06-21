5a1f6d8e0828dc0abba178b8fba3f58363998ab3
Remy Sharp <remy@remysharp.com>
2024-06-21 18:01:08 +0100

    feat: landing page
    Todo:
    
    - Load real worker image (and use OakImage)
    - Fix fonts - unsure why not loading
    - Discuss with Anna about font weights
    - Get real content from Emma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,223 @@
-import dynamic from 'next/dynamic';
-import { SwaggerUIProps } from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+import {
+  OakBox,
+  OakFlex,
+  OakGrid,
+  OakGridArea,
+  OakHeading,
+  OakLI,
+  OakLink,
+  OakMaxWidth,
+  OakP,
+  OakPrimaryButton,
+  OakSecondaryButton,
+  OakSpan,
+} from '@oaknational/oak-components';
+import styled from 'styled-components';
+import Feature from '~/components/Feature';
+import List from '~/components/List';
+import Logo from '~/components/Logo';
 
-const SwaggerUI = dynamic<SwaggerUIProps>(() => import('swagger-ui-react'), {
-  ssr: false,
-});
+const OakLinkLogo = styled(OakLink)`
+  margin: 0;
+  padding: 0;
+
+  span {
+    color: black;
+    display: flex;
+  }
+`;
+
+/**
+ * Notes:
+ * - font currently defaulting to helvetica in oak-components
+ */
 
 export default function Page() {
   return (
     <>
-      <style global jsx>{`
-        .swagger-ui .model {
-          font-size: 14px;
-          line-height: 1.3;
-        }
-        .swagger-ui .auth-btn-wrapper .btn-done {
-          margin-left: 12px;
-        }
-      `}</style>
-      <SwaggerUI url="/api/v0/swagger.json" />
+      <Banner />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </>
   );
+}
+
+function Banner() {
+  return (
+    <OakFlex
+      as="header"
+      $alignItems={'center'}
+      $gap={'all-spacing-10'}
+      $pv="inner-padding-s"
+      $ph="inner-padding-m"
+      $bb={'border-solid-m'}>
+      <OakLinkLogo href="https://www.thenational.academy/">
+        <Logo width="104" height="48" />
+      </OakLinkLogo>
+      <OakHeading tag="h1" $font="heading-6">
+        Oak OpenAPI
+      </OakHeading>
+    </OakFlex>
+  );
+}
+
+function Hero() {
+  return (
+    <OakBox $background="mint50">
+      <OakMaxWidth $flexDirection={'row'} $pv={'inner-padding-xl6'}>
+        <OakGrid $cg="space-between-m" $rg="space-between-m">
+          <OakGridArea
+            $colSpan={7}
+            $pa="inner-padding-xl"
+            $flexDirection={'column'}
+            $gap={'all-spacing-6'}>
+            <OakSpan
+              style={{ width: 'fit-content' }}
+              $background="mint110"
+              $borderRadius="border-radius-xl"
+              $pv="inner-padding-xs"
+              $ph="inner-padding-s"
+              $font={'body-3-bold'}>
+              Beta
+            </OakSpan>
+            <OakHeading $font="heading-2" tag="h2">
+              Oak OpenAPI
+            </OakHeading>
+            <OakHeading $font="heading-5" tag="h3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit
+            </OakHeading>
+            <OakP $color="black" $font="body-1" data-testId="OakP-id">
+              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip.
+            </OakP>
+            <OakFlex $gap={'all-spacing-4'}>
+              <OakSecondaryButton element="a" href="/request-api-key">
+                Request an API key
+              </OakSecondaryButton>
+              <OakPrimaryButton
+                element="a"
+                href="/playground"
+                isTrailingIcon={true}
+                iconName="external">
+                Visit OpenAPI playground
+              </OakPrimaryButton>
+            </OakFlex>
+          </OakGridArea>
+          <OakGridArea $colSpan={5}>
+            <OakFlex>
+              <img
+                // FIXME
+                src="/images/tmp-workers.png"
+                style={{ height: 'fit-content', width: '100%' }}
+                alt="placeholder"
+                width={2228}
+                height={1472}
+                sizes="400px"
+              />
+            </OakFlex>
+          </OakGridArea>
+        </OakGrid>
+      </OakMaxWidth>
+    </OakBox>
+  );
+}
+function Features() {
+  return (
+    <OakBox $background="white">
+      <OakMaxWidth $flexDirection={'row'} $pv={'inner-padding-xl6'}>
+        <OakGrid $cg="space-between-m" $rg="space-between-m">
+          <OakGridArea
+            $colSpan={4}
+            $pa="inner-padding-xl"
+            $gap={'all-spacing-5'}>
+            <OakHeading $font="heading-5" tag="h3">
+              What can I expect?
+            </OakHeading>
+            <OakP $font="body-1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate, minima. Doloremque nesciunt deserunt quos nisi unde,
+              eaque iste odio ex similique voluptatibus fugiat eius sed ut
+              reiciendis rem dolor voluptates.
+            </OakP>
+            <List>
+              <OakLI>List item 1</OakLI>
+              <OakLI>List item 2</OakLI>
+              <OakLI>List item 3</OakLI>
+              <OakLI>List item 4</OakLI>
+            </List>
+          </OakGridArea>
+          <OakGridArea
+            $colSpan={8}
+            $pa="inner-padding-xl"
+            $gap={'all-spacing-10'}>
+            <Feature
+              title="How do I get started with Oak OpenAPI?"
+              cta="Request an API key"
+              ctaHref="/request-api-key">
+              <OakP>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate, minima. Doloremque nesciunt deserunt quos nisi unde,
+                eaque iste odio ex similique voluptatibus fugiat eius sed ut
+                reiciendis rem dolor voluptates.
+              </OakP>
+            </Feature>
+            <Feature
+              title="What are we offering?"
+              cta="Visit OpenAPI playground"
+              ctaHref="/playground">
+              <OakP>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate, minima. Doloremque nesciunt deserunt quos nisi unde,
+                eaque iste odio ex similique voluptatibus fugiat eius sed ut
+                reiciendis rem dolor voluptates.
+              </OakP>
+            </Feature>
+            <Feature
+              title="Help us make Oak OpenAPI better"
+              cta="Give feedback here"
+              ctaHref="/feedback">
+              <OakP>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate, minima. Doloremque nesciunt deserunt quos nisi unde,
+                eaque iste odio ex similique voluptatibus fugiat eius sed ut
+                reiciendis rem dolor voluptates.
+              </OakP>
+            </Feature>
+          </OakGridArea>
+        </OakGrid>
+      </OakMaxWidth>
+    </OakBox>
+  );
+}
+function HowItWorks() {
+  return (
+    <OakBox $background="mint110">
+      <OakMaxWidth $flexDirection={'row'} $pv={'inner-padding-xl6'}>
+        <OakGrid $cg="space-between-m" $rg="space-between-m">
+          <OakGridArea
+            $colSpan={6}
+            $pa="inner-padding-xl"
+            $flexDirection={'column'}
+            $gap={'all-spacing-6'}>
+            <OakHeading $font="heading-5" tag="h3">
+              How our OpenAPI works
+            </OakHeading>
+            <OakP>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+              nobis officia dicta eveniet aliquid? Consequuntur, vel sed harum
+              cupiditate est voluptates itaque quasi, sapiente quos aspernatur
+              nobis rem, quaerat distinctio?
+            </OakP>
+          </OakGridArea>
+        </OakGrid>
+      </OakMaxWidth>
+    </OakBox>
+  );
+}
+
+function Footer() {
+  return <footer />;
 }
---
+++ src/pages/playground.tsx
@@ -0,0 +1,24 @@
+import dynamic from 'next/dynamic';
+import { SwaggerUIProps } from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+
+const SwaggerUI = dynamic<SwaggerUIProps>(() => import('swagger-ui-react'), {
+  ssr: false,
+});
+
+export default function Page() {
+  return (
+    <>
+      <style global jsx>{`
+        .swagger-ui .model {
+          font-size: 14px;
+          line-height: 1.3;
+        }
+        .swagger-ui .auth-btn-wrapper .btn-done {
+          margin-left: 12px;
+        }
+      `}</style>
+      <SwaggerUI url="/api/v0/swagger.json" />
+    </>
+  );
+}
