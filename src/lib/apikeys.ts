af9961074fb7d0032519c65b2bd3186073d9347c
Remy Sharp <remy@remysharp.com>
2024-10-18 13:26:06 +0100

    feat: restructure how users are stored (better)
    Additionally track simple request number.
    
    This also added in the 150 batch keys we needed for the hackathon.

--- src/lib/apikeys.ts
+++ src/lib/apikeys.ts
@@ -1,22 +1,69 @@
 import { redis } from '~/lib/redis';
 import { defaultRateLimit } from './rateLimit';
+import { v4 as uuid } from 'uuid';
 
 export type User = {
+  id: number;
   key: string;
-  name: string;
-  id: string;
-  email?: string;
+  name?: string | null;
+  email?: string | null;
   rateLimit?: number;
 };
 
-export async function findUserByKey(key: string): Promise<User | null> {
-  const res = (await redis.json.get(
-    'apikeys',
-    `$[?(@.key=="${key}")]`
-  )) as User[];
+export async function addUser({
+  email = null,
+  name = null,
+  rateLimit = defaultRateLimit,
+  key = uuid(),
+}: {
+  email?: null | string;
+  name?: null | string;
+  rateLimit?: number;
+  key?: string;
+} = {}): Promise<string> {
+  const userExists = await redis.exists(`user:${key}`);
 
-  if (res.length === 1) {
-    const user = res[0];
+  if (!userExists) {
+    const id = await redis.incr('next_user_id');
+
+    const user = {
+      id,
+      key,
+      email,
+      name,
+      rateLimit,
+      requests: 0,
+    };
+
+    // Store the user in Redis using key as the lookup identifier
+    await redis.hset(`user:${key}`, user);
+
+    if (email) {
+      await redis.set(`user:email:${email}`, key);
+    }
+  } else {
+    // If the user exists, update the fields without modifying the `id`
+    const updates = {
+      email: email,
+      name: name,
+      rateLimit: rateLimit || null,
+    };
+
+    // Perform an update for existing fields without touching `id`
+    await redis.hset(`user:${key}`, updates);
+  }
+
+  return key;
+}
+
+export async function findUserByKey(key: string): Promise<User | null> {
+  const user: User | null = await redis.hgetall(`user:${key}`);
+
+  if (user) {
+    // track how many requests in total (for a fast way to find busy
+    // or idle users)
+    await redis.hincrby(`user:${key}`, 'requests', 1);
+
     if (user.rateLimit === undefined) {
       user.rateLimit = defaultRateLimit;
     }
