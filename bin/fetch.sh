02b79863a32eb368350669c5f8a943e9ca4e3ee2
Remy Sharp <remy@remysharp.com>
2024-10-22 11:53:21 +0100

    chore: block english entirely
    victorian-childhood-non-fiction-reading-and-writing was ready, but not sure when to show

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -52,7 +52,15 @@ get "/key-stages/ks1/subject/maths/assets" '.code? != "NOT_FOUND"'
 title "# DOWNLOADS"
 
 get "/download/exploring-titles-in-unseen-poetry/type/video" '.code == "NOT_FOUND"'
+get "/download/child-workers-in-the-victorian-era/type/video" '.code == "NOT_FOUND"'
 get "/download/imagining-you-are-the-characters-the-three-billy-goats-gruff/type/video" ".code? | not"
 
+
+get "/download/the-power-of-rhetoric-in-writing/type/video" '.code == "NOT_FOUND"'
+
+get "/units/writing-rhetoric-1e51/summary" '.code == "NOT_FOUND"'
+get "/lessons/writing-an-effective-speech-using-rhetoric-6xj32r/summary" '.code == "NOT_FOUND"'
+get "/lessons/writing-an-effective-speech-using-rhetoric-6xj32r/assets" '.code == "NOT_FOUND"'
+get "/lessons/the-power-of-rhetoric-in-writing/assets" '.code == "NOT_FOUND"'
 
 title "FIN $TESTS tests run"
