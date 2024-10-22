cc665368dec6bde7102c0a695da55ff9c324c466
Remy Sharp <remy@remysharp.com>
2024-10-22 13:36:30 +0100

    fix: search with unit + ks

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -63,4 +63,6 @@ get "/lessons/writing-an-effective-speech-using-rhetoric-6xj32r/summary" '.code 
 get "/lessons/writing-an-effective-speech-using-rhetoric-6xj32r/assets" '.code == "NOT_FOUND"'
 get "/lessons/the-power-of-rhetoric-in-writing/assets" '.code == "NOT_FOUND"'
 
+get "/search/lessons?q=run&keyStage=ks1&subject=english" '.code? | not'
+
 title "FIN $TESTS tests run"
