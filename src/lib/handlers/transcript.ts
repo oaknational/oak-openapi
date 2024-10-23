e61219ff06723aced50055118a94664e483ef1ec
Remy Sharp <remy@remysharp.com>
2024-10-23 11:26:29 +0100

    docs: add example to transcript endpoint

--- src/lib/handlers/transcript.ts
+++ src/lib/handlers/transcript.ts
@@ -36,7 +36,11 @@ export const getLessonTranscript = router({
           request: {
             lesson: 'checking-understanding-of-basic-transformations',
           },
-          response: {},
+          response: {
+            transcript:
+              "Hello, I'm Mrs. Lashley. I'm looking forward to guiding you through your learning today...",
+            vtt: "WEBVTT\n\n1\n00:00:06.300 --> 00:00:08.070\n<v ->Hello, I'm Mrs. Lashley.</v>\n\n2\n00:00:08.070 --> 00:00:09.240\nI'm looking forward to guiding you\n\n3\n00:00:09.240 --> 00:00:10.980\nthrough your learning today...",
+          },
         },
       },
     })
