3956f0de04b125364b9a8564ca1460d86922d396
Remy Sharp <remy@remysharp.com>
2024-11-26 10:31:27 +0000

    feat: refactor to support updating keys
    Also all the hackathon keys have been updated

--- src/lib/apikeys.ts
+++ src/lib/apikeys.ts
@@ -8,24 +8,72 @@ export type User = {
   name?: string | null;
   email?: string | null;
   rateLimit?: number;
+  requests?: number;
 };
 
-export async function addUser({
-  email = null,
-  name = null,
-  company = null,
-  rateLimit = defaultRateLimit,
-  key = uuid(),
-}: {
+type UserUpdate = {
   email?: null | string;
   name?: null | string;
   company?: null | string;
   rateLimit?: number;
   key?: string;
-} = {}): Promise<string> {
-  const userExists = await redis.exists(`user:${key}`);
+};
+
+export async function updateUser(opts: {
+  key: string; // required
+  email?: string;
+  name?: null | string;
+  company?: null | string;
+  rateLimit?: number;
+}) {
+  if (!opts.key) {
+    throw new Error('opts.key is required');
+  }
+
+  const userExists = await redis.exists(`user:${opts.key}`);
 
   if (!userExists) {
+    throw new Error('User does not exist');
+  }
+
+  return manageUser(opts);
+}
+
+export function addUser(opts: UserUpdate = {}) {
+  return manageUser(opts);
+}
+
+async function manageUser({
+  email = null,
+  name = null,
+  company = null,
+  rateLimit = defaultRateLimit,
+  key = uuid(),
+}: UserUpdate = {}): Promise<string> {
+  const userExists = await redis.exists(`user:${key}`);
+
+  if (userExists) {
+    // If the user exists, update the fields without modifying the `key` field
+    const updates = {
+      email,
+      name,
+      company,
+      rateLimit: rateLimit || null,
+    };
+
+    if (email) {
+      // check if the email address has changed
+      // and if it has, update the index
+      const existingUser = await redis.hgetall(`user:${key}`);
+      if (existingUser && existingUser.email !== email) {
+        await redis.del(`user:email:${existingUser.email}`);
+      }
+      await redis.set(`user:email:${email}`, key);
+    }
+
+    // Perform an update for existing fields without touching `id`
+    await redis.hset(`user:${key}`, updates);
+  } else {
     const id = await redis.incr('next_user_id');
 
     const user = {
@@ -44,29 +92,23 @@ export async function addUser({
     if (email) {
       await redis.set(`user:email:${email}`, key);
     }
-  } else {
-    // If the user exists, update the fields without modifying the `id`
-    const updates = {
-      email: email,
-      name: name,
-      company: company,
-      rateLimit: rateLimit || null,
-    };
-
-    // Perform an update for existing fields without touching `id`
-    await redis.hset(`user:${key}`, updates);
   }
 
   return key;
 }
 
-export async function findUserByKey(key: string): Promise<User | null> {
+export async function findUserByKey(
+  key: string,
+  log: boolean = true
+): Promise<User | null> {
   const user: User | null = await redis.hgetall(`user:${key}`);
 
   if (user) {
     // track how many requests in total (for a fast way to find busy
     // or idle users)
-    await redis.hincrby(`user:${key}`, 'requests', 1);
+    if (log) {
+      await redis.hincrby(`user:${key}`, 'requests', 1);
+    }
 
     if (user.rateLimit === undefined) {
       user.rateLimit = defaultRateLimit;
