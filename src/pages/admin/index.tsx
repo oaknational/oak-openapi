9fd32a096ac5552851fbfe2eb094f7accc0ee3b4
Remy Sharp <remy@remysharp.com>
2024-11-27 19:49:57 +0000

    fix: add commit workflow
    Plus fix the linting issue, and try to catch before CI

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 import {
   OakBox,
   OakFlex,
@@ -13,7 +13,7 @@ import {
   OakSecondaryButton,
   OakFieldError,
 } from '@oaknational/oak-components';
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function AdminPage() {
   return (
@@ -25,30 +25,30 @@ export default function AdminPage() {
         <OakMaxWidth
           $ph="inner-padding-m"
           $flexDirection={'row'}
-          $pv={['inner-padding-xl2', 'inner-padding-xl6']}>
+          $pv={['inner-padding-xl2', 'inner-padding-xl6']}
+        >
           <OakGrid $cg="space-between-m" $rg="space-between-m">
             <OakGridArea
               $colSpan={[12, 3]}
               $alignSelf={'start'}
               $position={['static', 'sticky', 'sticky']}
               $top={'all-spacing-10'}
-              $display={['none', 'block', 'block']}>
-            </OakGridArea>
+              $display={['none', 'block', 'block']}
+            ></OakGridArea>
             <OakGridArea
               $colSpan={[12]}
               $position={['static', 'static', 'sticky']}
               $display={['block', 'none', 'none']}
               $pt="inner-padding-m"
               $pb="inner-padding-m"
-            >
-            </OakGridArea>
+            ></OakGridArea>
 
             <OakGridArea
               $width={'100%'}
               $colSpan={[12, 7]}
               $gap={'all-spacing-9'}
             >
-              <CreateAPIKey />  
+              <CreateAPIKey />
             </OakGridArea>
           </OakGrid>
         </OakMaxWidth>
@@ -63,7 +63,7 @@ function CreateAPIKey() {
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
   const [key, setKey] = useState('');
-  
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const response = await fetch('/api/admin/create-api-key', {
@@ -79,30 +79,62 @@ function CreateAPIKey() {
     } else {
       setKey(data.apiKey);
     }
-  }
+  };
 
-  return <>
-    <OakHeading tag="h2">Create an API key</OakHeading>
-    <OakFlex as="form" $flexDirection="column" $gap="all-spacing-8" onSubmit={handleSubmit}>
-      <div><OakLabel htmlFor="name">Individual's name</OakLabel>
-      <OakTextInput name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} /></div>
-      <div><OakLabel htmlFor="company">Company</OakLabel>
-      <OakTextInput name="company" id="company" value={company} onChange={(e) => setCompany(e.target.value)} /></div>
-      <div><OakLabel htmlFor="email">Email address</OakLabel>
-      <OakTextInput type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} /></div>
+  return (
+    <>
+      <OakHeading tag="h2">Create an API key</OakHeading>
+      <OakFlex
+        as="form"
+        $flexDirection="column"
+        $gap="all-spacing-8"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <OakLabel htmlFor="name">Individual&apos;s name</OakLabel>
+          <OakTextInput
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <OakLabel htmlFor="company">Company</OakLabel>
+          <OakTextInput
+            name="company"
+            id="company"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          />
+        </div>
+        <div>
+          <OakLabel htmlFor="email">Email address</OakLabel>
+          <OakTextInput
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-      <OakFlex $gap="all-spacing-4">
-        <OakPrimaryButton disabled={!!(error || key)} type="submit">
-          Generate API Key
-        </OakPrimaryButton>
+        <OakFlex $gap="all-spacing-4">
+          <OakPrimaryButton disabled={!!(error || key)} type="submit">
+            Generate API Key
+          </OakPrimaryButton>
 
-        <OakSecondaryButton onClick={() => window.location.reload()}>
-        Clear
-      </OakSecondaryButton>
+          <OakSecondaryButton onClick={() => window.location.reload()}>
+            Clear
+          </OakSecondaryButton>
+        </OakFlex>
+        {error && <OakFieldError>{error}</OakFieldError>}
+        {key && (
+          <OakP $font="body-1-bold" $color="success">
+            <strong>API Key:</strong> {key}
+          </OakP>
+        )}
       </OakFlex>
-      { error && <OakFieldError>{error}</OakFieldError> }
-      { key && <OakP $font="body-1-bold" $color="success"><strong>API Key:</strong> {key}</OakP> }
-
-    </OakFlex>
-  </>
+    </>
+  );
 }
