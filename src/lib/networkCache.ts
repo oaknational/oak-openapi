7f6926bcc81bbfb3428d66465cf8110ad192b6c5
Remy Sharp <remy@remysharp.com>
2024-11-28 16:50:12 +0000

    fix: reintroduce the cache-control public
    https://datatracker.ietf.org/doc/html/rfc7234#section-3.2
    
    suggests CDN can cache when `public` is included

--- src/lib/networkCache.ts
+++ src/lib/networkCache.ts
@@ -1,4 +1,4 @@
-// const stale_while_revalidate_seconds = 60 * 3; // 3 minutes
+const stale_while_revalidate_seconds = 60 * 3; // 3 minutes
 const s_maxage_seconds = 60 * 60 * 24; // 1 day
 
 import { t } from '~/lib/trpc';
@@ -8,7 +8,7 @@ export const defaultCaching = t.middleware(({ ctx, next }) => {
     throw new Error('Response object does not support setting headers');
   }
 
-  // const cacheControl = `public, durable, s-maxage=${s_maxage_seconds}, stale-while-revalidate=${stale_while_revalidate_seconds}`;
+  const cacheControl = `public, durable, s-maxage=${s_maxage_seconds}, stale-while-revalidate=${stale_while_revalidate_seconds}`;
 
   // add vercel specific cache header
   ctx.res.setHeader(
@@ -20,7 +20,7 @@ export const defaultCaching = t.middleware(({ ctx, next }) => {
   ctx.res.setHeader('CDN-Cache-Control', `public, max-age=${s_maxage_seconds}`);
 
   // add client header, though in reality this will have little impact
-  // ctx.res.setHeader('Cache-Control', cacheControl);
+  ctx.res.setHeader('Cache-Control', cacheControl);
 
   return next();
 });
