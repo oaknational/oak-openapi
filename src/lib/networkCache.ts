02981529096048d3c578c8bf08b0456cefbca181
Remy Sharp <remy@remysharp.com>
2024-11-29 13:18:37 +0000

    fix: try to use CF cache

--- src/lib/networkCache.ts
+++ src/lib/networkCache.ts
@@ -1,5 +1,5 @@
-const stale_while_revalidate_seconds = 60 * 3; // 3 minutes
-const s_maxage_seconds = 60 * 60 * 24; // 1 day
+// const s_maxage_seconds = 60 * 60 * 24; // 1 day
+const maxage_seconds = 60 * 10; // 10 minutes for testing
 
 import { t } from '~/lib/trpc';
 
@@ -8,19 +8,19 @@ export const defaultCaching = t.middleware(({ ctx, next }) => {
     throw new Error('Response object does not support setting headers');
   }
 
-  const cacheControl = `public, durable, s-maxage=${s_maxage_seconds}, stale-while-revalidate=${stale_while_revalidate_seconds}`;
+  const cacheControl = `public, max-age=${maxage_seconds}`;
+
+  // this header is crafted for cloudflare to pick up and store on the cdn.
+  ctx.res.setHeader('Cache-Control', cacheControl);
 
   // add vercel specific cache header
-  ctx.res.setHeader(
-    'Vercel-CDN-Cache-Control',
-    `public, max-age=${s_maxage_seconds}`,
-  );
+  // ctx.res.setHeader(
+  //   'Vercel-CDN-Cache-Control',
+  //   `public, max-age=${s_maxage_seconds}`,
+  // );
 
   // also apply "generic" CDN cache header
-  ctx.res.setHeader('CDN-Cache-Control', `public, max-age=${s_maxage_seconds}`);
-
-  // add client header, though in reality this will have little impact
-  ctx.res.setHeader('Cache-Control', cacheControl);
+  // ctx.res.setHeader('CDN-Cache-Control', `public, max-age=${s_maxage_seconds}`);
 
   return next();
 });
