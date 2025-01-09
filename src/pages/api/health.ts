ab34d0488ae30db68ae06cd1bb6885f585061ef5
Remy Sharp <remy@remysharp.com>
2025-01-09 12:52:09 +0000

    feat: check all the MVs are still present in the health check
    Ref CUR-1065

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,5 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getClient, gql, views } from '~/lib/owaClient';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ status: 'ok' });
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const client = getClient();
+  const query = gql`
+    query {
+    ${views
+      .map((view) => {
+        return `
+          ${view}(limit: 1) {
+            __typename
+          }
+      `;
+      })
+      .join('\n')}
+    }
+  `;
+
+  try {
+    await client.request(query); // if the MV is missing then this will throw an error
+    res.status(200).json({ status: 'ok' });
+  } catch (_) {
+    res.status(500).json({ status: 'fail' });
+  }
 }
