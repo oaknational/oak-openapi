c4b807b18f35c4260156eb6bfd03fc3c81eb0c27
Remy Sharp <remy@remysharp.com>
2025-01-21 16:22:26 +0000

    fix: support unit variants in cycle 2

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { makeCaller } from './helper';
+import { makeCaller, makeRes } from './helper';
 
 test('get cycle 2 (2024-2025) unit', async () => {
   const caller = makeCaller({
@@ -10,17 +10,29 @@ test('get cycle 2 (2024-2025) unit', async () => {
     unit: 'ceramics-cake-culture',
   });
 
-  expect(Array.isArray(res)).toBe(false); // we're expecting 1
+  expect(Array.isArray(res)).toBe(false);
 });
 
-test('optionality unit', async () => {
+test('optionality unit 2023-24 cohort', async () => {
   const caller = makeCaller({
     user: 1,
   });
 
-  const res = await caller.getUnits.getUnit({
-    unit: 'life-in-a-capital-city-london-cardiff-775',
+  const unit = 'life-in-a-capital-city-london-cardiff-775';
+  const res = await caller.getUnits.getUnit({ unit });
+  expect(res).toHaveProperty('unitSlug');
+  expect(res.unitSlug).toBe(unit);
+});
+
+test('optionality unit 2024-25 cohort', async () => {
+  const request = makeRes();
+  const caller = makeCaller({
+    user: 1,
+    request,
   });
 
-  expect(Array.isArray(res)).toBe(false); // we're expecting 1
+  const unit = 'iterative-design-nature-4776';
+  const res = await caller.getUnits.getUnit({ unit });
+  expect(res).toHaveProperty('unitSlug');
+  expect(res.unitSlug).toBe(unit);
 });
