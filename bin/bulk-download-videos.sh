76becc900b516158f076a7645c27e13df67de276
Remy Sharp <remy@remysharp.com>
2025-03-26 20:43:24 +0000

    feat: remove the tar'ing - taking too long
    This part will have to be in "post"

--- bin/bulk-download-videos.sh
+++ bin/bulk-download-videos.sh
@@ -5,18 +5,26 @@ TMP_DIR="/tmp/downloads"
 
 mkdir -p "$TMP_DIR"
 
-tail -n0 -F "$WATCH_FILE" | while IFS= read -r line; do
-  [ -z "$line" ] && continue
+while true; do
+  if [ -f "$WATCH_FILE" ]; then
+    line=$(head -n 1 "$WATCH_FILE")
+  else
+    sleep 1
+    continue
+  fi
+
+  [ -z "$line" ] && sleep 1 && continue
+
+  echo "$line"
 
   url=$(printf '%s' "$line" | cut -f1)
   filename=$(printf '%s' "$line" | cut -f2)
-  tarball=$(printf '%s' "$line" | cut -f3)
-  tmpfile="$TMP_DIR/$filename"
+  dir=$(printf '%s' "$line" | cut -f3)
+  outdir="out/$dir/videos"
+  mkdir -p "$outdir"
+  tmpfile="$outdir/$filename"
 
-  echo "Downloading $filename from $url to $tmpfile and appending to $tarball"
 
-  wget -q -O "$tmpfile" "$url" && \
-    tar --append --file="$tarball" -C "$TMP_DIR" "$filename" && \
-    rm "$tmpfile" && \
+  wget -q "$url" -O "$tmpfile" && \
     sed -i.bak "\|$line|d" "$WATCH_FILE"
 done
