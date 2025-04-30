cbcfe883b152219abd87ceec160c1b52c3e02f0d
Remy Sharp <remy@remysharp.com>
2025-04-30 13:37:26 +0100

    test: check rate limits, custom, unlimited + default work

--- __tests__/helper.ts
+++ __tests__/helper.ts
@@ -1,7 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import router from '~/lib/router';
-import { createCallerFactory } from '~/lib/trpc';
-import { vitest, vi } from 'vitest';
+import { vi } from 'vitest';
+export * from './make-call';
 
 vi.mock('~/lib/rateLimit', async (importOriginal: () => Promise<object>) => {
   const actual = await importOriginal();
@@ -16,41 +14,3 @@ vi.mock('~/lib/rateLimit', async (importOriginal: () => Promise<object>) => {
     }),
   };
 });
-
-export function makeRes() {
-  return {
-    writeHead: vitest.fn(),
-    setHeader: vitest.fn(),
-    getHeader: vitest.fn(),
-    pipe: vitest.fn(),
-    on: vitest.fn(),
-    once: vitest.fn(),
-    emit: vitest.fn(),
-    write: vitest.fn(),
-    end: vitest.fn(),
-  };
-}
-
-export function makeCaller(opts = {}, res = makeRes()) {
-  const createCaller = createCallerFactory(router);
-  const callerOptions = {
-    req: {} as NextApiRequest,
-    res: res as unknown as NextApiResponse,
-    rateLimit: undefined,
-    user: null,
-    ...opts,
-  };
-
-  return createCaller(callerOptions);
-}
-
-export function authedCaller(user = 1) {
-  const res = makeRes();
-  return {
-    caller: makeCaller({
-      user,
-      res,
-    }),
-    request: res,
-  };
-}
