9975f7ddefad49dfa7ac2864df34765b396a08a8
Remy Sharp <remy@remysharp.com>
2025-05-21 10:26:21 +0100

    chore: wip

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,68 +1,163 @@
-import { OakAnchorTarget, OakLI } from '@oaknational/oak-components';
-import React from 'react';
 import styled from 'styled-components';
+import { OakAPINavigationLink } from './OakAPINavigationLink';
+import {
+  OakFlex,
+  OakHeading,
+  OakIcon,
+  OakLabel,
+  OakLink,
+  OakMaxWidth,
+  OakModal,
+} from '@oaknational/oak-components';
+import Logo from '~/components/Logo';
 
-const StyledNav = styled.nav`
-  outline: none;
-`;
-const StyledOakLink = styled.a`
-  color: #222222;
-  display: Flex;
-`;
-
-const StyledUL = styled.ul`
-  list-style: none;
-  padding: 0;
+const OakLinkLogo = styled(OakLink)`
   margin: 0;
-  text-decoration: none;
-`;
+  padding: 0;
 
-const StyledOLItem = styled(OakLI)`
-  position: relative;
-  counter-increment: list-counter;
-  display: flex;
-  align-items: center;
-  color: #222222;
-  margin-bottom: 20px;
-
-  min-height: 40px;
-  &:last-child {
-    margin-bottom: 0;
-  }
-
-  &:hover {
-    text-decoration: underline;
-    color: #575757;
+  span {
+    display: flex;
   }
 `;
 
-export type NavProps = {
-  title?: string;
-  items: { title: string; href: string }[];
-  ariaLabel?: string;
-  anchorTarget?: string;
-  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
-};
+const BurgerMenu = styled.div`
+  label {
+    cursor: pointer;
+  }
 
-export const Nav = ({
-  items,
-  ariaLabel,
-  anchorTarget,
-  onClick,
-  ...rest
-}: NavProps) => {
+  input,
+  label {
+    display: none;
+  }
+
+  @media (max-width: 1280px) {
+    label {
+      display: block;
+    }
+
+    nav {
+      display: none;
+    }
+
+    input:checked + nav {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      background-color: white;
+      padding: 16px;
+      justify-content: flex-start;
+
+      & > div {
+        flex-direction: column;
+
+        > a {
+          display: inline-block;
+          width: fit-content;
+        }
+      }
+    }
+  }
+`;
+
+export function Navigation() {
   return (
-    <StyledNav aria-label={ariaLabel} {...rest}>
-      {anchorTarget && <OakAnchorTarget id={anchorTarget} />}
-      <StyledUL role="list">
-        {items.map((item, index) => (
-          <StyledOLItem $font={'heading-6'} key={index}>
-            <StyledOakLink onClick={onClick} href={item.href}>
-              {item.title}
-            </StyledOakLink>
-          </StyledOLItem>
-        ))}
-      </StyledUL>
-    </StyledNav>
+    <OakFlex $bb={'border-solid-m'}>
+      <OakMaxWidth
+        as="header"
+        $alignItems={'center'}
+        $gap={'all-spacing-10'}
+        $pv="inner-padding-s"
+        $ph="inner-padding-m"
+        $color="text-primary"
+        $justifyContent="space-between"
+        $flexDirection={'row'}
+      >
+        <OakFlex role="list" $gap="space-between-l" $alignItems="center">
+          <OakFlex $alignItems="center" $gap="space-between-s">
+            <OakLinkLogo
+              aria-label="Oak National Academy"
+              href="https://www.thenational.academy/"
+            >
+              <Logo width="31" height="42" />
+            </OakLinkLogo>
+            <OakHeading ariaHidden tag="h1" $font="heading-6">
+              Oak Curriculum API
+            </OakHeading>
+          </OakFlex>
+        </OakFlex>
+        <Menu>
+          <OakFlex role="list" $gap="space-between-m">
+            <OakAPINavigationLink role="listitem" href="#" className="selected">
+              Home
+            </OakAPINavigationLink>
+            <OakAPINavigationLink role="listitem" href="/docs">
+              Documentation
+            </OakAPINavigationLink>
+          </OakFlex>
+          <OakFlex role="list" $gap="space-between-m">
+            <OakAPINavigationLink
+              role="listitem"
+              href="mailto:xxx@yyy.com"
+              isTrailingIcon
+              iconName="send"
+            >
+              Request an API key
+            </OakAPINavigationLink>
+
+            <OakAPINavigationLink
+              role="listitem"
+              href="/playground"
+              isTrailingIcon
+              iconName="external"
+              target="_blank"
+            >
+              API playground
+            </OakAPINavigationLink>
+
+            <OakAPINavigationLink
+              role="listitem"
+              href="/bulk-downloads"
+              isTrailingIcon
+              iconName="external"
+              target="_blank"
+            >
+              Bulk downloads
+            </OakAPINavigationLink>
+          </OakFlex>
+        </Menu>
+      </OakMaxWidth>
+    </OakFlex>
   );
-};
+}
+
+function Menu({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <OakLabel htmlFor="show-menu">
+        <OakIcon $colorFilter="black" alt="Show menu" iconName="hamburger" />
+      </OakLabel>
+      <input id="show-menu" className="show-menu" type="checkbox" />
+      <OakModal
+        isOpen={false}
+        isLeftHandSide={false}
+        footerSlot={null}
+        onClose={() => {}}
+      >
+        <BurgerMenu>
+          <OakFlex
+            as="nav"
+            $gap="space-between-l"
+            $justifyContent="space-between"
+            $flexGrow="1"
+          >
+            {children}
+          </OakFlex>
+        </BurgerMenu>
+      </OakModal>
+    </>
+  );
+}
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,17 +1,21 @@
-import { OakFlex, OakHeading, OakLink } from '@oaknational/oak-components';
-import styled from 'styled-components';
-import Logo from '~/components/Logo';
 import Footer from '~/components/Footer';
 import Head from 'next/head';
-import { OakAPINavigationLink } from '~/components/OakAPINavigationLink';
+import { Navigation } from '~/components/Nav';
+import {
+  OakBox,
+  OakFlex,
+  OakImage,
+  OakMaxWidth,
+} from '@oaknational/oak-components';
+import styled from 'styled-components';
 
-const OakLinkLogo = styled(OakLink)`
-  margin: 0;
-  padding: 0;
-
-  span {
-    color: black;
-    display: flex;
+const BlockHeading = styled.h2`
+  em {
+    display: inline-block;
+    background-color: #bef2bd; /* FIXME: use oak token */
+    padding: 0.1em 0.2em;
+    text-decoration: none;
+    font-style: normal;
   }
 `;
 
@@ -21,73 +25,45 @@ export default function Page() {
       <Head>
         <title>Oak Curriculum API - Oak National Academy</title>
       </Head>
-      <Banner />
+      <Navigation />
+      <OakMaxWidth
+        $ph="inner-padding-m"
+        $flexDirection={'row'}
+        $pv="inner-padding-xl6"
+      >
+        <BlockAndText></BlockAndText>
+      </OakMaxWidth>
       <Footer />
     </>
   );
 }
 
-function Banner() {
+function BlockAndText() {
   return (
-    <OakFlex
-      as="header"
-      $alignItems={'center'}
-      $gap={'all-spacing-10'}
-      $pv="inner-padding-s"
-      $ph="inner-padding-m"
-      $bb={'border-solid-m'}
-      $color="text-primary"
-      $justifyContent="space-around"
-    >
-      <OakFlex role="list" $gap="space-between-l" $alignItems="center">
-        <OakFlex $alignItems="center" $gap="space-between-s">
-          <OakLinkLogo
-            aria-label="Oak National Academy"
-            href="https://www.thenational.academy/"
-          >
-            <Logo width="31" height="42" />
-          </OakLinkLogo>
-          <OakHeading ariaHidden tag="h1" $font="heading-6">
-            Oak Curriculum API
-          </OakHeading>
-        </OakFlex>
-        <OakAPINavigationLink role="listitem" href="#" className="selected">
-          Home
-        </OakAPINavigationLink>
-        <OakAPINavigationLink role="listitem" href="/docs">
-          Documentation
-        </OakAPINavigationLink>
-      </OakFlex>
-      <OakFlex role="list" $gap="space-between-m">
-        <OakAPINavigationLink
-          role="listitem"
-          href="mailto:xxx@yyy.com"
-          isTrailingIcon
-          iconName="send"
-        >
-          Request an API key
-        </OakAPINavigationLink>
-
-        <OakAPINavigationLink
-          role="listitem"
-          href="/playground"
-          isTrailingIcon
-          iconName="external"
-          target="_blank"
-        >
-          API playground
-        </OakAPINavigationLink>
-
-        <OakAPINavigationLink
-          role="listitem"
-          href="/bulk-downloads"
-          isTrailingIcon
-          iconName="external"
-          target="_blank"
-        >
-          Bulk downloads
-        </OakAPINavigationLink>
-      </OakFlex>
+    <OakFlex>
+      <OakBox>
+        <BlockHeading>
+          <em>Integrate</em> Oak&apos;s high-quality educational content into
+          your service
+        </BlockHeading>
+        <p>
+          We&apos;re offering a free API to share our high-quality educational
+          content with the broader education community, all under the Open
+          Government Licence.
+        </p>
+        <p>
+          For more information, please refer to the{' '}
+          <a href="/docs">documentation</a>.
+        </p>
+      </OakBox>
+      <OakBox>
+        <OakImage
+          sizes="width: 2228px, height: 1472px"
+          src={{ src: '/images/api_1.png', width: 2228, height: 1472 }}
+          alt=""
+          $height="all-spacing-20"
+        ></OakImage>
+      </OakBox>
     </OakFlex>
   );
 }
