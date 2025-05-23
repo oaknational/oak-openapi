1ff97a26bf9eb795f61b7f75dcae19099b518423
Remy Sharp <remy@remysharp.com>
2025-05-23 10:57:09 +0100

    fix: send dummy value to redis if missing
    This gets the bulk export working since it doesn't use redis
    
    Also updated code to use `with` not `assert`

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
+  url: process.env.UPSTASH_REDIS_REST_URL || 'http://dummy:1234',
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
