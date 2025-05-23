02c0e0f85606f5582d457429a73f088d63750fb1
Remy Sharp <remy@remysharp.com>
2025-05-23 12:51:43 +0100

    fix: empty redis token

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,5 +2,5 @@ import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL || 'http://dummy:1234',
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || 'dummy',
 });
