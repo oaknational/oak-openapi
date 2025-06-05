846aeed386fa2a45d43e2caea06bc43d529f417d
Sonali Singh <sonali.singh@live.co.uk>
2025-06-05 17:47:19 +0100

    feat: update health and admin routes

--- src/app/api/pingdom/route.ts
+++ src/app/api/pingdom/route.ts
@@ -1,8 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { getClient, gql, views } from '~/lib/owaClient';
 import { performance } from 'node:perf_hooks';
+import { NextResponse } from 'next/server';
+import { Context } from '~/lib/context';
 
-export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+export default async function handler({ resHeaders }: Context) {
   const start = performance.now();
   const client = getClient();
   const query = gql`
@@ -19,22 +20,32 @@ export default async function handler(_: NextApiRequest, res: NextApiResponse) {
     }
   `;
 
-  res.setHeader('Content-Type', 'application/xml');
+  resHeaders.set('Content-Type', 'application/xml');
 
   try {
     await client.request(query); // if the MV is missing then this will throw an error
     const end = performance.now();
     const duration = end - start;
-    res
-      .status(200)
-      .send(
-        `<pingdom_http_custom_check><status>OK</status><response_time>${duration.toFixed(3)}</response_time></pingdom_http_custom_check>`,
-      );
+    const response = new NextResponse(
+      `<pingdom_http_custom_check><status>OK</status><response_time>${duration.toFixed(3)}</response_time></pingdom_http_custom_check>`,
+      { status: 200, statusText: 'ok' },
+    );
+    return response;
   } catch (_) {
-    res
-      .status(200)
-      .send(
-        '<pingdom_http_custom_check><status>failure</status></pingdom_http_custom_check>',
-      );
+    const response = new NextResponse(
+      '<pingdom_http_custom_check><status>failure</status></pingdom_http_custom_check>',
+      { status: 200, statusText: 'failure' },
+    );
+    return response;
   }
 }
+
+export {
+  handler as GET,
+  handler as POST,
+  handler as PUT,
+  handler as PATCH,
+  handler as DELETE,
+  handler as OPTIONS,
+  handler as HEAD,
+};
