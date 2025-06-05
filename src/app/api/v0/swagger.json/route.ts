f18bf283a5a812c9b05b274ac6b4712e2545b623
Sonali Singh <sonali.singh@live.co.uk>
2025-06-05 18:34:18 +0100

    chore: remove unused imports

--- src/app/api/v0/swagger.json/route.ts
+++ src/app/api/v0/swagger.json/route.ts
@@ -17,14 +17,6 @@ const bearerAuth = {
   bearerFormat: 'JWT', // Optional, only if you want to specify the token format
 } as const;
 
-const httpMethods = [
-  'get',
-  'post',
-  'put',
-  'patch',
-  'delete',
-] as OpenAPIV3.HttpMethods[];
-
 export const openApiDocument = generateOpenApiDocument(router, {
   title: 'Oak OpenAPI',
   version: process.env.VERCEL_GIT_COMMIT_SHA
@@ -45,102 +37,102 @@ openApiDocument.security = [{ bearerAuth: [] }];
 // note that the trpc-openapi module doesn't quite make this compatible with GPT
 // so I'm going to re-work the schema returned by this endpoint to make it compatible
 
-function camelCase(str: string, lcFirst = false): string {
-  const res = str
-    .split(/[^a-z0-9]+/i)
-    .filter(Boolean)
-    .map((_: string) => _[0].toUpperCase() + _.slice(1))
-    .join('');
+// function camelCase(str: string, lcFirst = false): string {
+//   const res = str
+//     .split(/[^a-z0-9]+/i)
+//     .filter(Boolean)
+//     .map((_: string) => _[0].toUpperCase() + _.slice(1))
+//     .join('');
 
-  if (!lcFirst) {
-    return res;
-  }
+//   if (!lcFirst) {
+//     return res;
+//   }
 
-  return res[0]?.toLowerCase() + res.slice(1);
-}
+//   return res[0]?.toLowerCase() + res.slice(1);
+// }
 
 // sort the paths, because it's a nice thing to do
-openApiDocument.paths = Object.keys(openApiDocument.paths)
-  .sort((a, b) => {
-    return a.localeCompare(b);
-  })
-  .reduce((result, key) => {
-    result[key] = openApiDocument.paths[key];
-    return result;
-  }, {} as OpenAPIV3.PathsObject);
+// openApiDocument.paths = Object.keys(openApiDocument.paths)
+//   .sort((a, b) => {
+//     return a.localeCompare(b);
+//   })
+//   .reduce((result, key) => {
+//     result[key] = openApiDocument.paths[key];
+//     return result;
+//   }, {} as OpenAPIV3.PathsObject);
 
-Object.keys(openApiDocument.paths).forEach((_) => {
-  const path = openApiDocument.paths[_];
+// Object.keys(openApiDocument.paths).forEach((_) => {
+//   const path = openApiDocument.paths[_];
 
-  if (path === undefined) {
-    return;
-  }
+//   if (path === undefined) {
+//     return;
+//   }
 
-  for (const method of httpMethods) {
-    const root = path[method];
+//   for (const method of httpMethods) {
+//     const root = path[method];
 
-    if (!root) {
-      continue;
-    }
+//     if (!root) {
+//       continue;
+//     }
 
-    if (root.operationId) {
-      // the dash in the operationId is causing problems with openai's GPT -
-      // resulting in the GPT service not being able to parse the path or method
-      root.operationId = camelCase(root.operationId.replace(/-/g, ' '), true);
-    }
+//     if (root.operationId) {
+//       // the dash in the operationId is causing problems with openai's GPT -
+//       // resulting in the GPT service not being able to parse the path or method
+//       root.operationId = camelCase(root.operationId.replace(/-/g, ' '), true);
+//     }
 
-    const responseObject = root.responses;
+//     const responseObject = root.responses;
 
-    const id = camelCase(_);
+//     const id = camelCase(_);
 
-    Object.keys(responseObject).map((code: string) => {
-      const responseId = `${id}Response${code}`;
-      const response = responseObject[code];
+//     Object.keys(responseObject).map((code: string) => {
+//       const responseId = `${id}Response${code}`;
+//       const response = responseObject[code];
 
-      if (!response) {
-        return;
-      }
+//       if (!response) {
+//         return;
+//       }
 
-      // Weird test because TypeScript doesn't handle `if (response.$ref)`
-      if ('$ref' in response) {
-        // skip existing references
-        return;
-      }
+//       // Weird test because TypeScript doesn't handle `if (response.$ref)`
+//       if ('$ref' in response) {
+//         // skip existing references
+//         return;
+//       }
 
-      if (!response.content) {
-        return;
-      }
+//       if (!response.content) {
+//         return;
+//       }
 
-      const schema = response.content['application/json']?.schema;
+//       const schema = response.content['application/json']?.schema;
 
-      if (!schema) {
-        return;
-      }
+//       if (!schema) {
+//         return;
+//       }
 
-      if (response.content['application/json']) {
-        response.content['application/json'].schema = {
-          $ref: `#/components/schemas/${id}`,
-        };
-      }
+//       if (response.content['application/json']) {
+//         response.content['application/json'].schema = {
+//           $ref: `#/components/schemas/${id}`,
+//         };
+//       }
 
-      if (!openApiDocument.components) {
-        openApiDocument.components = {};
-      }
+//       if (!openApiDocument.components) {
+//         openApiDocument.components = {};
+//       }
 
-      if (!openApiDocument.components.schemas) {
-        openApiDocument.components.schemas = {};
-      }
+//       if (!openApiDocument.components.schemas) {
+//         openApiDocument.components.schemas = {};
+//       }
 
-      if (!openApiDocument.components.responses) {
-        openApiDocument.components.responses = {};
-      }
+//       if (!openApiDocument.components.responses) {
+//         openApiDocument.components.responses = {};
+//       }
 
-      openApiDocument.components.schemas[id] = schema;
-      openApiDocument.components.responses[responseId] = response;
-      responseObject[code] = { $ref: `#/components/responses/${responseId}` };
-    });
-  }
-});
+//       openApiDocument.components.schemas[id] = schema;
+//       openApiDocument.components.responses[responseId] = response;
+//       responseObject[code] = { $ref: `#/components/responses/${responseId}` };
+//     });
+//   }
+// });
 
 export async function GET() {
   return NextResponse.json(openApiDocument);
