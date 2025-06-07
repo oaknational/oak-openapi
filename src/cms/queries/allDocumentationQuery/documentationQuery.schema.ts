f1f31fb7aa9ca3992dacf189c8f6c54f5d5ca523
Sonali Singh <sonali.singh@live.co.uk>
2025-06-07 16:56:44 +0100

    feat: docs page sanity set up

---
+++ src/cms/queries/allDocumentationQuery/documentationQuery.schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { portableTextContentSchema } from '../../schemaTypes/shared/portableText.schema';
+
+export const documentationQuerySchema = z.array(
+  z.object({
+    title: z.string(),
+    navGroupType: z.object({
+      slug: z.object({ current: z.string() }),
+      name: z.string(),
+    }),
+    contentRaw: portableTextContentSchema,
+  }),
+);
+export type DocumentationQuery = z.infer<typeof documentationQuerySchema>;
