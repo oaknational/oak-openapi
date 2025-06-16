c8623029a4af910a9d1a3f0aafe9f9ea7fd7ff83
Sonali Singh <sonali.singh@live.co.uk>
2025-06-16 13:51:30 +0100

    feat: created docs layout structure

--- src/app/(pages)/docs/layout.tsx
+++ src/app/(pages)/docs/layout.tsx
@@ -1,32 +1,12 @@
-// 'use client';
-import Footer from '@/components/Footer';
-import Banner from '@/components/landingPage/Banner';
-// import documentationBySlugQuery from '@/cms/queries/documentationBySlugQuery/documentationBySlugQuery.query';
 import navDocsListQuery from '@/cms/queries/navDocsListQuery/navDocsListQuery.query';
-// import Nav from '@/components/landingPage/Nav';
 
-// import { OakBox, OakMaxWidth } from '@oaknational/oak-components';
-import DocsNav from '@/components/documentationPages/DocsNav';
-
-type DocsLayoutProps = { navGroup: string; slug: string };
+import DocsLayout from './DocsLayout';
 
 export default async function Layout({
-  params,
   children,
 }: {
-  params: Promise<DocsLayoutProps>;
   children: React.ReactNode;
 }) {
-  const { navGroup, slug } = await params;
-  const docs = await navDocsListQuery();
-
-  console.log(docs, navGroup, slug);
-  return (
-    <>
-      <Banner />
-      <DocsNav items={docs.items} />
-      {children}
-      <Footer />
-    </>
-  );
+  const { items } = await navDocsListQuery();
+  return <DocsLayout children={children} docsPageListItems={items} />;
 }
