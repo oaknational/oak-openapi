0aebaca9f52b31fb60c928397fff8de5d2fa1d06
Sonali Singh <sonali.singh@live.co.uk>
2025-06-16 16:22:16 +0100

    refactor: sequences into examples / schemas folder

--- src/lib/handlers/sequences/sequences.ts
+++ src/lib/handlers/sequences/sequences.ts
@@ -1,7 +1,6 @@
 import toSorted from 'array.prototype.tosorted';
 import { protectedProcedure } from '@/lib/protect';
 import { router } from '@/lib/trpc';
-import { z } from 'zod';
 import {
   getClient,
   gql,
@@ -9,15 +8,25 @@ import {
   SequenceView,
   sequenceView,
   sequenceViewWhereInput,
-} from '../owaClient';
-import { parseSubjectPhaseSlug } from '../sequenceSlugParser';
-import { examBoards } from '../oakConsts';
-import { blockedSequenceSubjects } from '../blockedContent';
+} from '../../owaClient';
+import { parseSubjectPhaseSlug } from '../../sequenceSlugParser';
+import { examBoards } from '../../oakConsts';
+import { blockedSequenceSubjects } from '../../blockedContent';
 import { TRPCError } from '@trpc/server';
+import { sequenceUnitsRequestSchema } from './schemas/sequenceUnitsRequest.schema';
+import {
+  Category,
+  ExamSubjectsWithoutTiers,
+  ExamSubjectsWithTiers,
+  SequenceSchema,
+  sequenceUnitsResponseSchema,
+  YearSequence,
+  Tier,
+} from './schemas/sequenceUnitsResponse.schema';
 
 toSorted.shim();
 
-const years = [
+export const years = [
   '1',
   '2',
   '3',
@@ -31,119 +40,6 @@ const years = [
   '11',
   'all-years',
 ];
-
-const input = z.object({
-  sequence: z.string(),
-
-  year: z.enum(years as [string]).optional(),
-});
-
-const categorySchema = z.object({
-  categoryTitle: z.string(),
-  categorySlug: z.string().optional(),
-});
-
-const threadSchema = z.object({
-  threadTitle: z.string(),
-  threadSlug: z.string(),
-  order: z.number(),
-});
-
-const unitOptionSchema = z.object({
-  unitTitle: z.string(),
-  unitSlug: z.string(),
-});
-
-const unitWithOptionsSchema = z.object({
-  unitTitle: z.string(),
-  unitOrder: z.number(),
-  unitOptions: z.array(unitOptionSchema),
-  categories: z.array(categorySchema).optional(),
-  threads: z.array(threadSchema).optional(),
-});
-
-const unitNoOptionsSchema = z.object({
-  unitTitle: z.string(),
-  unitOrder: z.number(),
-  unitSlug: z.string(),
-  categories: z.array(categorySchema).optional(),
-  threads: z.array(threadSchema).optional(),
-});
-
-const unitSchema = z.union([unitWithOptionsSchema, unitNoOptionsSchema]);
-
-const tierSchema = z.object({
-  tierTitle: z.string(),
-  tierSlug: z.string(),
-  units: z.array(unitSchema),
-});
-
-const examSubjectsSchemaWithTiers = z.object({
-  examSubjectTitle: z.string(),
-  examSubjectSlug: z.string().optional(),
-  tiers: z.array(tierSchema),
-});
-
-const examSubjectsSchemaWithoutTiers = z.object({
-  examSubjectTitle: z.string(),
-  examSubjectSlug: z.string().optional(),
-  units: z.array(unitSchema),
-});
-
-const yearSequenceKS4WithExamSubjectsSchema = z.object({
-  year: z.number(),
-  title: z
-    .string({
-      description: 'Optional alternative title for the year sequence',
-    })
-    .optional(),
-  examSubjects: z.array(
-    z.union([examSubjectsSchemaWithTiers, examSubjectsSchemaWithoutTiers]),
-  ),
-});
-
-const yearSequenceKS4WithoutExamSubjectsSchema = z.object({
-  year: z.number(),
-  title: z
-    .string({
-      description: 'Optional alternative title for the year sequence',
-    })
-    .optional(),
-  tiers: z.array(tierSchema),
-});
-
-const yearSequenceSchema = z.object({
-  year: z.union([z.number(), z.literal('all-years')]),
-  title: z
-    .string({
-      description: 'Optional alternative title for the year sequence',
-    })
-    .optional(),
-  units: z.array(unitSchema),
-});
-
-const sequenceSchema = z.union([
-  yearSequenceSchema,
-  yearSequenceKS4WithExamSubjectsSchema,
-  yearSequenceKS4WithoutExamSubjectsSchema,
-]);
-
-const output = z.array(sequenceSchema);
-
-export type SequenceSchema = z.infer<typeof sequenceSchema>;
-export type YearSequence = z.infer<typeof yearSequenceSchema>;
-export type ExamSubjectsWithTiers = z.infer<typeof examSubjectsSchemaWithTiers>;
-export type ExamSubjectsWithoutTiers = z.infer<
-  typeof examSubjectsSchemaWithoutTiers
->;
-type Tier = z.infer<typeof tierSchema>;
-type Category = z.infer<typeof categorySchema>;
-export type yearSequenceKS4WithExamSubjects = z.infer<
-  typeof yearSequenceKS4WithExamSubjectsSchema
->;
-export type UnitWithOptions = z.infer<typeof unitWithOptionsSchema>;
-export type UnitWithoutOptions = z.infer<typeof unitNoOptionsSchema>;
-export type Unit = z.infer<typeof unitSchema>;
 
 type WhereCondition = {
   _and: Array<{
@@ -241,39 +137,10 @@ export const getSequences = router({
         method: 'GET',
         path: '/sequences/{sequence}/units',
         description: '',
-        // example: {
-        //   request: {
-        //     sequence: 'english-primary',
-        //   },
-        //   response: [
-        //     {
-        //       year: 1,
-        //       units: [
-        //         {
-        //           unitTitle: 'Speaking and Listening',
-        //           unitOrder: 1,
-        //           unitSlug: 'speaking-and-listening',
-        //           categories: [
-        //             {
-        //               categoryTitle: 'Reading, writing & oracy',
-        //             },
-        //           ],
-        //           threads: [
-        //             {
-        //               threadTitle: 'Developing spoken language',
-        //               threadSlug: 'developing-spoken-language',
-        //               order: 8,
-        //             },
-        //           ],
-        //         },
-        //       ],
-        //     },
-        //   ],
-        // },
       },
     })
-    .input(input)
-    .output(output)
+    .input(sequenceUnitsRequestSchema)
+    .output(sequenceUnitsResponseSchema)
     .query(async ({ input }) => {
       const client = getClient();
 
