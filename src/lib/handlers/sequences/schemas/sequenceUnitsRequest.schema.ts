0657c8ca146f260c26045a6b9df625146eeaf0ee
Sonali Singh <sonali.singh@live.co.uk>
2025-06-16 16:44:22 +0100

    refactor: add years to types

--- src/lib/handlers/sequences/schemas/sequenceUnitsRequest.schema.ts
+++ src/lib/handlers/sequences/schemas/sequenceUnitsRequest.schema.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod';
-import { years } from '../sequences';
+import { years } from '@/lib/types';
 
 export const sequenceUnitsRequestSchema = z.object({
   sequence: z.string(),
-
   year: z.enum(years as [string]).optional(),
 });
