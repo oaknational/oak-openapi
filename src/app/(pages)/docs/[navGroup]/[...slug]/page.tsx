98bd285fe542ccc789680cd4e3771beaf907d7ad
Sonali Singh <sonali.singh@live.co.uk>
2025-06-16 13:52:40 +0100

    feat: added content from sanity into docs page

--- src/app/(pages)/docs/[navGroup]/[...slug]/page.tsx
+++ src/app/(pages)/docs/[navGroup]/[...slug]/page.tsx
@@ -1,37 +1,18 @@
-// import { OakBox } from '@oaknational/oak-components';
+import documentationBySlugQuery from '@/cms/queries/documentationBySlugQuery/documentationBySlugQuery.query';
+import MainDocsContent from '@/components/documentationPages/MainDocsContent';
 
-import React from 'react';
-import navDocsListQuery from '@/cms/queries/navDocsListQuery/navDocsListQuery.query';
-// import DocsNav from '@/components/documentationPages/DocsNav';
-import DocPage from './doc';
-// import documentationQuery from '@/cms/queries/allDocumentationQuery/documentation.query';
-// import ContentPortableText from '@/cms/sanityResolvers/ContentPortableText';
-
-// import { DocumentationQuery } from '@/cms/queries/allDocumentationQuery/documentationQuery.schema';
-// import Link from 'next/link';
-// import documentationBySlugQuery from '@/cms/queries/documentationBySlugQuery/documentationBySlugQuery.query';
-
-export default async function DocsPage(
-  params: Promise<{
-    navGroup: string;
-    slug: string;
-  }>,
-) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ navGroup: string; slug: string[] }>;
+}) {
   const { navGroup, slug } = await params;
-  const { items } = await navDocsListQuery();
-  // const documentationData = await documentationBySlugQuery(navGroup, slug);
 
-  // documentationData.forEach((doc) => {
-  //   console.log(doc.contentRaw);
-  // });
+  const documentationData = await documentationBySlugQuery(navGroup, slug[0]);
+
   return (
     <>
-      <p>{navGroup}</p>
-      <p>{slug}</p>
-      <DocPage items={items} content={[]} />
-      {/* <Link href="/docs/about-oaks-api" /> */}
-      {/* <MainDoc
-      <Main docs={documentationData} /> */}
+      <MainDocsContent docs={documentationData} />
     </>
   );
 }
--- src/components/documentationPages/MainDocsContent.tsx
+++ src/components/documentationPages/MainDocsContent.tsx
@@ -1,12 +1,8 @@
 'use client';
-import { OakBox } from '@oaknational/oak-components';
+import { OakBox, OakHeading } from '@oaknational/oak-components';
 
-import React from 'react';
-
-// import documentationQueryByNavGroup from '@/cms/queries/documentationQueryByNavGroup/documentation.query';
 import ContentPortableText from '@/cms/sanityResolvers/ContentPortableText';
 import { DocumentationContentPageBlock } from '@/cms/schemaTypes';
-// import { PortableTextJSON } from '@/cms/schemaTypes/shared/portableText.schema';
 
 type CMSDocumentationProps = {
   docs: DocumentationContentPageBlock[];
@@ -17,6 +13,14 @@ export default function MainDocsContent({ docs }: CMSDocumentationProps) {
     <OakBox>
       {docs && (
         <OakBox>
+          <OakHeading
+            ariaHidden
+            tag="h1"
+            $font="heading-5"
+            $pa={'space-between-xl'}
+          >
+            {docs[0].title}
+          </OakHeading>
           {docs.map((doc) => (
             <ContentPortableText
               key={doc.title}
