c1160a053943d9455cb37fff48b93e3b09586514
Sonali Singh <sonali.singh@live.co.uk>
2025-06-16 13:51:51 +0100

    feat: added redirect to default docs page

--- src/app/(pages)/docs/page.tsx
+++ src/app/(pages)/docs/page.tsx
@@ -1,20 +1,5 @@
-// // 'use client';
-// import { OakBox } from '@oaknational/oak-components';
-// import React from 'react';
-// import documentationQuery from '@/cms/queries/allDocumentationQuery/documentation.query';
-// import ContentPortableText from '@/cms/sanityResolvers/ContentPortableText';
+import { redirect } from 'next/navigation';
 
-// import { DocumentationQuery } from '@/cms/queries/allDocumentationQuery/documentationQuery.schema';
-// // import Link from 'next/link';
-// import DocsNav from '@/components/documentationPages/DocsNav';
-// import navDocsListQuery from '@/cms/queries/navDocsListQuery/navDocsListQuery.query';
-// import Link from 'next/link';
-
-export default async function DocsLandingPage() {
-  // const { items } = await navDocsListQuery();
-  return <>{/* <Link href="/about-oaks-api/what-is-oaks-api" /> */}</>;
+export default async function Page() {
+  redirect('/docs/about-oaks-api/what-is-oaks-api');
 }
-
-// type CMSDocumentationProps = {
-//   docs: DocumentationQuery;
-// };
