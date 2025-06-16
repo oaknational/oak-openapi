5eaba7fdabad0469fd302baadc61a0f5063c7708
Sonali Singh <sonali.singh@live.co.uk>
2025-06-16 18:11:19 +0100

    feat: migrate questions for key stage and subject endpoint

--- src/lib/zod-openapi/generated/questions/index.ts
+++ src/lib/zod-openapi/generated/questions/index.ts
@@ -1,2 +1,4 @@
+export * from './questionsForKeyStageAndSubjectRequest.openapi';
+export * from './questionsForKeyStageAndSubjectResponse.openapi';
 export * from './questionsForSequenceRequest.openapi';
 export * from './questionsForSequenceResponse.openapi';
