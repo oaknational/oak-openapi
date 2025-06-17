a27719739ce500eb09171999acd1c9ff8199eebc
Sonali Singh <sonali.singh@live.co.uk>
2025-06-17 16:47:46 +0100

    refactor: naming for changelog latest schema

--- src/lib/handlers/changelog/schemas/changelogResponse.schema.ts
+++ src/lib/handlers/changelog/schemas/changelogResponse.schema.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod';
-import { changelogLatestSchema } from './changelogLatest.schema';
+import { changelogLatestSchema } from './changelogLatestResponse.schema';
 
 export const changelogResponseSchema = z.array(changelogLatestSchema);
