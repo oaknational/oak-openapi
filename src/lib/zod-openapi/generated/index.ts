859b434e24a569a73aa6f126970895c56702cb39
Sonali Singh <sonali.singh@live.co.uk>
2025-06-17 17:57:54 +0100

    feat: migrate keyStageSubjectLessons endpoint

--- src/lib/zod-openapi/generated/index.ts
+++ src/lib/zod-openapi/generated/index.ts
@@ -1,5 +1,6 @@
 export * as allKeyStageAndSubjectUnits from './allKeyStageAndSubjectUnits';
 export * as assets from './assets';
+export * as keyStageSubjectLessons from './keyStageSubjectLessons';
 export * as keyStages from './keyStages';
 export * as lesson from './lesson';
 export * as questions from './questions';
