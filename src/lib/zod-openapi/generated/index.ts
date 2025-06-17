ae126993ac7f87ebd5f3d8cc038c0b6be60653f8
Sonali Singh <sonali.singh@live.co.uk>
2025-06-17 16:52:26 +0100

    feat: migrate key stages endpoint

--- src/lib/zod-openapi/generated/index.ts
+++ src/lib/zod-openapi/generated/index.ts
@@ -2,5 +2,6 @@ export * as all from './all';
 export * as lesson from './lesson';
 export * as sequence from './sequence';
 export * as subject from './subject';
+export * as key from './key';
 export * as question from './question';
 export * as questions from './questions';
