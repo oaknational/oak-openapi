bc6dc239cc9c17d60b0d2ca6a4df2f7662e3dfa8
Remy Sharp <remy@remysharp.com>
2025-07-04 18:09:36 +0100

    feat: landing page wired to sanity + transform

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,6 +1,7 @@
-'use client';
+import getCMSContent from '@/cms/queries/allCurriculumApiLandingPage/landingPage.query';
 import LandingPage from '@/components/landingPage/LandingPage';
 
-export default function Page() {
-  return <LandingPage />;
+export default async function Page() {
+  const documentationData = await getCMSContent();
+  return <LandingPage documentationData={documentationData} />;
 }
