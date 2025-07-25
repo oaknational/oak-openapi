bdb82dd6d8c0aef6957fa268f183df2e73ed955b
Amy Matthews <amy.matthews@thenational.academy>
2025-07-25 14:50:44 +0100

    feat: a new module for GCP Cloud Run job

--- infrastructure/bulk-upload/jobs.tf
+++ infrastructure/bulk-upload/jobs.tf
@@ -1,79 +1,40 @@
-locals {
-  application_name_gc = lower(replace(local.application_name, " ", "-"))
-}
+module "job" {
+  source = "github.com/oaknational/oak-terraform-modules//modules/gcp_job?ref=v1.3.0"
 
-resource "google_cloud_run_v2_job" "this" {
-  name = "ow-${local.env}-ldn-${local.application_name_gc}"
-
-  location            = var.region
-  deletion_protection = false
-
-  template {
-    template {
-      containers {
-        image = "europe-west2-docker.pkg.dev/oak-national-academy-ci-cd/oak-open-api/bulk-data:${var.tag_id}"
-
-        env {
-          name  = "OAK_GRAPHQL_HOST"
-          value = var.graphql_host_url
-        }
-
-        env {
-          name  = "OAK_GRAPHQL_SECRET"
-          value = var.graphql_host_secret
-        }
-
-        env {
-          name  = "OAK_AUTH_TYPE"
-          value = var.oak-auth-type
-        }
-
-        env {
-          name  = "HASURA_ROLE"
-          value = var.hasura-role
-        }
-
-        env {
-          name  = "BUCKET_NAME"
-          value = google_storage_bucket.this.name
-        }
-
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "1024Mi"
-          }
-        }
-      }
-      timeout = "600s"
-    }
+  name_parts = {
+    domain = "ow"
+    app    = local.application_name_gc
   }
-}
 
-resource "google_cloud_scheduler_job" "this" {
-  count = local.env == "prod" ? 1 : 0
+  env          = local.env
+  docker_image = "europe-west2-docker.pkg.dev/oak-national-academy-ci-cd/oak-open-api/bulk-data:${var.tag_id}"
 
-  name        = "ow-${local.env}-ldn-${local.application_name_gc}-cron"
-  description = "Do a daily update of ${local.env} ${local.application_name}"
-  schedule    = "30 2 * * *"
-  time_zone   = "Europe/London"
+  service_account_email = data.terraform_remote_state.google_project.outputs.project_config.service_accounts["openapi-bulk-uploader-${local.project_env}"]
 
-  region = var.region
-
-  http_target {
-    http_method = "POST"
-    uri = join("", [
-      "https://",
-      google_cloud_run_v2_job.this.location,
-      "-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/",
-      google_cloud_run_v2_job.this.project,
-      "/jobs/",
-      google_cloud_run_v2_job.this.name,
-      ":run"
-    ])
-    oauth_token {
-      service_account_email = data.terraform_remote_state.google_project.outputs.project_config.service_accounts["openapi-bulk-uploader-${local.project_env}"]
-      scope                 = "https://www.googleapis.com/auth/cloud-platform"
+  environment_variables = [
+    {
+      name  = "OAK_GRAPHQL_HOST"
+      value = var.graphql_host_url
+    },
+    {
+      name  = "OAK_GRAPHQL_SECRET"
+      value = var.graphql_host_secret
+    },
+    {
+      name  = "OAK_AUTH_TYPE"
+      value = var.oak-auth-type
+    },
+    {
+      name  = "HASURA_ROLE"
+      value = var.hasura-role
+    },
+    {
+      name  = "BUCKET_NAME"
+      value = google_storage_bucket.this.name
     }
-  }
+  ]
+
+  schedule = local.env == "prod" ? {
+    crons = ["30 2 * * *"]
+  } : null
 }
