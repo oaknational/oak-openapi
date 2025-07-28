49ece8b3e8a8501de8090cb90fbc54f28907274b
Sonali Singh <sonali.singh@live.co.uk>
2025-07-28 15:00:35 +0100

    feat: get endpoint groups into the nav

--- src/cms/queries/navDocsListQuery/navDocsListQuery.query.ts
+++ src/cms/queries/navDocsListQuery/navDocsListQuery.query.ts
@@ -7,6 +7,42 @@ import {
 } from '@/cms/schemaTypes/curriculumApiDocsNav.schema';
 import query from './navDocsListQuery.gql';
 
+const API_ENDPOINTS_SLUG = 'api-endpoints';
+
+const endpointSlugs = {
+  lists: {
+    title: 'Lists',
+    slug: 'lists',
+  },
+  lessonData: {
+    title: 'Lesson data',
+    slug: 'lesson-data',
+  },
+  unitAndCurriculumData: {
+    title: 'Unit and curriculum data',
+    slug: 'unit-curriculum-data',
+  },
+  quizQuestions: {
+    title: 'Quiz questions',
+    slug: 'quiz-questions',
+  },
+  search: {
+    title: 'Search',
+    slug: 'search',
+  },
+};
+
+const getEndpointsNavData = () => {
+  return Object.values(endpointSlugs).map((page, order) => {
+    const { title, slug }: { title: string; slug: string } = page;
+
+    return {
+      title: `${order + 1}. ${title}`,
+      href: `${API_ENDPOINTS_SLUG}/${slug}`,
+    };
+  });
+};
+
 const navDocsListQuery = async () => {
   const res = await client.request(query);
   const { groups, pages } = res as NavDocsListQuery;
@@ -31,6 +67,11 @@ const navDocsListQuery = async () => {
           title: page.title,
           href: `${groupSlug}/${page.slug.text}`,
         }));
+
+      if (groupSlug === API_ENDPOINTS_SLUG) {
+        pages.push(...getEndpointsNavData());
+      }
+
       return {
         title: group.title,
         pages,
