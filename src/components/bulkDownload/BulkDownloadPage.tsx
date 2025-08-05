8004ee17a5e9556391e03e5707d8f805b07dd122
Remy Sharp <remy@remysharp.com>
2025-08-05 18:16:39 +0100

    feat: add new graph query for subjects + lesson count

--- src/components/bulkDownload/BulkDownloadPage.tsx
+++ src/components/bulkDownload/BulkDownloadPage.tsx
@@ -4,9 +4,10 @@ import Head from 'next/head';
 import { Navigation } from '../Nav';
 import { MaxWidth } from '../MaxWidth';
 import Footer from '../Footer';
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useState } from 'react';
 import {
   OakBox,
+  OakFlex,
   OakGrid,
   OakHeading,
   OakImage,
@@ -17,6 +18,7 @@ import {
 import styled from 'styled-components';
 import SelectCard from './SelectCard';
 import type { Subjects } from '@/app/(pages)/bulk-download/page';
+import CheckBox from '../CheckBox';
 
 interface BulkDownloadPageProps {
   subjects: Subjects;
@@ -26,7 +28,9 @@ type SelectionState = Record<string, { primary: boolean; secondary: boolean }>;
 
 type Action =
   | { type: 'TOGGLE_PRIMARY'; payload: string }
-  | { type: 'TOGGLE_SECONDARY'; payload: string };
+  | { type: 'TOGGLE_SECONDARY'; payload: string }
+  | { type: 'SET_ALL_PRIMARY'; payload: boolean }
+  | { type: 'SET_ALL_SECONDARY'; payload: boolean };
 
 const selectionReducer = (state: SelectionState, action: Action) => {
   switch (action.type) {
@@ -46,6 +50,16 @@ const selectionReducer = (state: SelectionState, action: Action) => {
           secondary: !state[action.payload]?.secondary,
         },
       };
+    case 'SET_ALL_PRIMARY':
+      return Object.keys(state).reduce((acc, slug) => {
+        acc[slug] = { ...state[slug], primary: action.payload };
+        return acc;
+      }, {} as SelectionState);
+    case 'SET_ALL_SECONDARY':
+      return Object.keys(state).reduce((acc, slug) => {
+        acc[slug] = { ...state[slug], secondary: action.payload };
+        return acc;
+      }, {} as SelectionState);
     default:
       return state;
   }
@@ -70,11 +84,29 @@ const ProperUL = styled(OakUL)`
 `;
 
 export default function BulkDownloadPage({ subjects }: BulkDownloadPageProps) {
-  const [selectedSubjects, dispatch] = useReducer(selectionReducer, {});
+  const initialSelectionState = subjects.reduce((acc, subject) => {
+    acc[subject.slug] = { primary: false, secondary: false };
+    return acc;
+  }, {} as SelectionState);
+
+  const [selectedSubjects, dispatch] = useReducer(
+    selectionReducer,
+    initialSelectionState,
+  );
+  const [allPrimaryChecked, setAllPrimaryChecked] = useState(false);
+  const [allSecondaryChecked, setAllSecondaryChecked] = useState(false);
 
   useEffect(() => {
-    console.log('Selected Subjects:', selectedSubjects);
-  }, [selectedSubjects]);
+    const allPrimary = subjects.every(
+      (subject) => selectedSubjects[subject.slug]?.primary,
+    );
+    setAllPrimaryChecked(allPrimary);
+
+    const allSecondary = subjects.every(
+      (subject) => selectedSubjects[subject.slug]?.secondary,
+    );
+    setAllSecondaryChecked(allSecondary);
+  }, [selectedSubjects, subjects]);
 
   const handlePrimaryChange = (subjectSlug: string) => {
     dispatch({ type: 'TOGGLE_PRIMARY', payload: subjectSlug });
@@ -82,6 +114,14 @@ export default function BulkDownloadPage({ subjects }: BulkDownloadPageProps) {
 
   const handleSecondaryChange = (subjectSlug: string) => {
     dispatch({ type: 'TOGGLE_SECONDARY', payload: subjectSlug });
+  };
+
+  const handleSelectAllPrimary = (checked: boolean) => {
+    dispatch({ type: 'SET_ALL_PRIMARY', payload: checked });
+  };
+
+  const handleSelectAllSecondary = (checked: boolean) => {
+    dispatch({ type: 'SET_ALL_SECONDARY', payload: checked });
   };
 
   return (
@@ -103,6 +143,7 @@ export default function BulkDownloadPage({ subjects }: BulkDownloadPageProps) {
           $background="mint"
           $cg="space-between-xxxl"
           $gridTemplateColumns="1fr 1fr"
+          $borderRadius="border-radius-m"
         >
           <OakBox>
             <OakHeading $mv="space-between-m" tag="h1" $font="heading-2">
@@ -137,30 +178,47 @@ export default function BulkDownloadPage({ subjects }: BulkDownloadPageProps) {
           </OakBox>
         </OakGrid>
 
-        <OakHeading tag="h2" $font="heading-3">
-          Download
-        </OakHeading>
-        <OakGrid
-          $rg="space-between-s"
-          $cg="space-between-s"
-          $gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-        >
-          {subjects.map((subject) => (
-            <SelectCard
-              key={subject.slug}
-              subject={subject.title}
-              primaryFileSize="100 MB"
-              secondaryFileSize="50 MB"
-              iconName={`subject-${subject.slug}`}
-              primaryChecked={selectedSubjects[subject.slug]?.primary || false}
-              secondaryChecked={
-                selectedSubjects[subject.slug]?.secondary || false
-              }
-              onPrimaryChange={() => handlePrimaryChange(subject.slug)}
-              onSecondaryChange={() => handleSecondaryChange(subject.slug)}
+        <OakFlex $gap="all-spacing-7" $flexDirection="column">
+          <OakHeading tag="h2" $font="heading-3">
+            Download
+          </OakHeading>
+          <OakFlex $gap="all-spacing-6" $flexDirection="row">
+            <CheckBox
+              label="Select all primary"
+              checked={allPrimaryChecked}
+              onChange={handleSelectAllPrimary}
             />
-          ))}
-        </OakGrid>
+            <CheckBox
+              label="Select all secondary"
+              checked={allSecondaryChecked}
+              onChange={handleSelectAllSecondary}
+            />
+          </OakFlex>
+
+          <OakGrid
+            $rg="space-between-s"
+            $cg="space-between-s"
+            $gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+          >
+            {subjects.map((subject) => (
+              <SelectCard
+                key={subject.slug}
+                subject={subject.title}
+                primaryLessonCount={subject.primary}
+                secondaryLessonCount={subject.secondary}
+                iconName={`subject-${subject.slug}`}
+                primaryChecked={
+                  selectedSubjects[subject.slug]?.primary || false
+                }
+                secondaryChecked={
+                  selectedSubjects[subject.slug]?.secondary || false
+                }
+                onPrimaryChange={() => handlePrimaryChange(subject.slug)}
+                onSecondaryChange={() => handleSecondaryChange(subject.slug)}
+              />
+            ))}
+          </OakGrid>
+        </OakFlex>
       </MaxWidth>
       <Footer />
     </>
