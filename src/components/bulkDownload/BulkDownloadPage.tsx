b693da9d69b788a5ffa0c7e76782793e19f431d8
Remy Sharp <remy@remysharp.com>
2025-08-05 17:42:22 +0100

    feat(wip): page rendering all the subjects + checkboxes

--- src/components/bulkDownload/BulkDownloadPage.tsx
+++ src/components/bulkDownload/BulkDownloadPage.tsx
@@ -4,15 +4,86 @@ import Head from 'next/head';
 import { Navigation } from '../Nav';
 import { MaxWidth } from '../MaxWidth';
 import Footer from '../Footer';
+import { useReducer, useEffect } from 'react';
 import {
   OakBox,
   OakGrid,
   OakHeading,
   OakImage,
+  OakLI,
   OakP,
+  OakUL,
 } from '@oaknational/oak-components';
+import styled from 'styled-components';
+import SelectCard from './SelectCard';
+import type { Subjects } from '@/app/(pages)/bulk-download/page';
 
-export default function Page() {
+interface BulkDownloadPageProps {
+  subjects: Subjects;
+}
+
+type SelectionState = Record<string, { primary: boolean; secondary: boolean }>;
+
+type Action =
+  | { type: 'TOGGLE_PRIMARY'; payload: string }
+  | { type: 'TOGGLE_SECONDARY'; payload: string };
+
+const selectionReducer = (state: SelectionState, action: Action) => {
+  switch (action.type) {
+    case 'TOGGLE_PRIMARY':
+      return {
+        ...state,
+        [action.payload]: {
+          ...state[action.payload],
+          primary: !state[action.payload]?.primary,
+        },
+      };
+    case 'TOGGLE_SECONDARY':
+      return {
+        ...state,
+        [action.payload]: {
+          ...state[action.payload],
+          secondary: !state[action.payload]?.secondary,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+const WhiteBoxHeading = styled(OakHeading)`
+  display: inline-block;
+  background: white;
+  padding: 6px 4px;
+  margin-left: -4px;
+  margin-top: -6px;
+`;
+
+const ProperUL = styled(OakUL)`
+  list-style-type: disc;
+  margin: 1em 0;
+  padding-left: 1.5rem;
+
+  li {
+    display: list-item;
+  }
+`;
+
+export default function BulkDownloadPage({ subjects }: BulkDownloadPageProps) {
+  const [selectedSubjects, dispatch] = useReducer(selectionReducer, {});
+
+  useEffect(() => {
+    console.log('Selected Subjects:', selectedSubjects);
+  }, [selectedSubjects]);
+
+  const handlePrimaryChange = (subjectSlug: string) => {
+    dispatch({ type: 'TOGGLE_PRIMARY', payload: subjectSlug });
+  };
+
+  const handleSecondaryChange = (subjectSlug: string) => {
+    dispatch({ type: 'TOGGLE_SECONDARY', payload: subjectSlug });
+  };
+
   return (
     <>
       <Head>
@@ -23,7 +94,7 @@ export default function Page() {
         $ph="inner-padding-m"
         $flexDirection={'column'}
         $pv="inner-padding-xl6"
-        $gap="all-spacing-16"
+        $gap="all-spacing-12"
         $color={'black'}
       >
         <OakGrid
@@ -34,19 +105,27 @@ export default function Page() {
           $gridTemplateColumns="1fr 1fr"
         >
           <OakBox>
-            <OakHeading $mv="space-between-m" tag="h2" $font="heading-2">
+            <OakHeading $mv="space-between-m" tag="h1" $font="heading-2">
               Bulk Download
             </OakHeading>
             <OakP $mb="space-between-m">
-              Oak’s lesson and curriculum text-based data is provided as a
+              Oak's lesson and curriculum text-based data is provided as a
               single JSON for each national curriculum subject and educational
               phase.
             </OakP>
+            <WhiteBoxHeading tag="h2" $font="heading-5">
+              What's included?
+            </WhiteBoxHeading>
+            <ProperUL $font="list-item-1">
+              <OakLI>Quiz questions and answers </OakLI>
+              <OakLI>Teaching transcript</OakLI>
+              <OakLI>Misconceptions</OakLI>
+              <OakLI>And much more....</OakLI>
+            </ProperUL>
           </OakBox>
           <OakBox>
             <OakImage
               sizes="width: 2228px, height: 1472px"
-              priority={true}
               src={{
                 src: '/images/bulk-hero.png',
                 width: 2228,
@@ -56,6 +135,31 @@ export default function Page() {
               $height="all-spacing-20"
             />
           </OakBox>
+        </OakGrid>
+
+        <OakHeading tag="h2" $font="heading-3">
+          Download
+        </OakHeading>
+        <OakGrid
+          $rg="space-between-s"
+          $cg="space-between-s"
+          $gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+        >
+          {subjects.map((subject) => (
+            <SelectCard
+              key={subject.slug}
+              subject={subject.title}
+              primaryFileSize="100 MB"
+              secondaryFileSize="50 MB"
+              iconName={`subject-${subject.slug}`}
+              primaryChecked={selectedSubjects[subject.slug]?.primary || false}
+              secondaryChecked={
+                selectedSubjects[subject.slug]?.secondary || false
+              }
+              onPrimaryChange={() => handlePrimaryChange(subject.slug)}
+              onSecondaryChange={() => handleSecondaryChange(subject.slug)}
+            />
+          ))}
         </OakGrid>
       </MaxWidth>
       <Footer />
---
+++ src/components/bulkDownload/SelectCard.tsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import CheckBox from '../CheckBox';
+import {
+  OakFlex,
+  OakSpan,
+  OakIcon,
+  OakIconName,
+} from '@oaknational/oak-components';
+
+interface SelectCardProps {
+  subject: string;
+  primaryFileSize: string;
+  secondaryFileSize: string;
+  primaryChecked?: boolean;
+  secondaryChecked?: boolean;
+  onPrimaryChange: () => void;
+  onSecondaryChange: () => void;
+  iconName: string; // Changed to string to allow dynamic icon names
+}
+
+const SelectCard: React.FC<SelectCardProps> = ({
+  subject,
+  primaryFileSize,
+  secondaryFileSize,
+  primaryChecked = false,
+  secondaryChecked = false,
+  onPrimaryChange,
+  onSecondaryChange,
+  iconName,
+}) => {
+  return (
+    <OakFlex
+      $display="flex"
+      $flexDirection="column"
+      $justifyContent="space-between"
+      $width="100%"
+      $borderRadius="border-radius-m"
+      $background="mint50"
+      $overflow="hidden"
+    >
+      <OakFlex
+        $flexDirection="column"
+        $alignItems="center"
+        $width="100%"
+        $gap="all-spacing-2"
+        $pa="all-spacing-4"
+        $pb="all-spacing-5"
+        $borderRadius="border-radius-m"
+      >
+        <OakIcon
+          $width="all-spacing-10"
+          $height={'all-spacing-10'}
+          iconName={iconName as OakIconName}
+        />
+
+        <OakSpan $font="heading-5" $textAlign="center" $color="black">
+          {subject}
+        </OakSpan>
+      </OakFlex>
+      <OakFlex
+        $flexDirection="row"
+        $gap="inner-padding-s"
+        $pa="all-spacing-4"
+        $pt="all-spacing-0"
+        $background="mint50"
+        $borderRadius="border-radius-m"
+      >
+        <OakFlex $flexDirection="column" $gap="all-spacing-2" $width="100%">
+          <DownloadOption
+            heading="Primary"
+            label={primaryFileSize}
+            checked={primaryChecked}
+            onChange={onPrimaryChange}
+          />
+          <DownloadOption
+            heading="Secondary"
+            label={secondaryFileSize}
+            checked={secondaryChecked}
+            onChange={onSecondaryChange}
+          />
+        </OakFlex>
+      </OakFlex>
+    </OakFlex>
+  );
+};
+
+interface DownloadOptionProps {
+  heading: string;
+  label: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+function DownloadOption({
+  heading,
+  label,
+  checked,
+  onChange,
+}: DownloadOptionProps) {
+  return (
+    <label>
+      <OakFlex
+        $flexDirection="row"
+        $alignItems="center"
+        $width="100%"
+        $gap="all-spacing-4"
+        $pa="all-spacing-3"
+        $background="white"
+        $ba="border-solid-s"
+        $borderColor="grey40"
+        $borderRadius="border-radius-s"
+      >
+        <OakFlex
+          $flexDirection="column"
+          $justifyContent="center"
+          $width="100%"
+          $gap="all-spacing-1"
+        >
+          <OakSpan $font="body-2-bold" $color="black">
+            {heading}
+          </OakSpan>
+          <OakSpan $font="body-3" $color="grey60">
+            {label}
+          </OakSpan>
+        </OakFlex>
+        <CheckBox checked={checked} onChange={onChange} />
+      </OakFlex>
+    </label>
+  );
+}
+
+export default SelectCard;
