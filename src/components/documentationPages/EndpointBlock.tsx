e2c64481a18e3b28be8c6ee60daae7c6d72190d2
Sonali Singh <sonali.singh@live.co.uk>
2025-08-12 14:33:06 +0100

    refactor: endpoint block into component

---
+++ src/components/documentationPages/EndpointBlock.tsx
@@ -0,0 +1,123 @@
+'use client';
+import { OakBox, OakFlex, OakHeading, OakP } from '@oaknational/oak-components';
+import styled from 'styled-components';
+import { Table } from '../Table';
+import { Roboto_Mono } from 'next/font/google';
+import { capitalize } from 'lodash';
+import { DocumentationContentPageBlock } from '@/cms/schemaTypes';
+import { Code } from '../Code';
+
+const robotoMono = Roboto_Mono({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  display: 'swap',
+  weight: ['400'],
+});
+
+export type InputOutputTableRow = {
+  name: string;
+  type: string;
+  description?: string;
+  example?: string;
+  required?: boolean;
+};
+
+export type InputOutputTable = InputOutputTableRow[];
+
+export type EndpointInfo = {
+  order: number;
+  title: string;
+  requestType: string;
+  description?: string;
+  path: string;
+  params?: InputOutputTable;
+  paramTypes: string[] | [];
+  output: InputOutputTable;
+  sampleResponse?: string;
+  slug: string;
+};
+
+export type EndpointDocsProps = {
+  title: string;
+  slug: string;
+  endpoints: EndpointInfo[];
+  docs: DocumentationContentPageBlock[];
+};
+
+const EndpointHeading = styled(OakHeading)`
+  color: var(--Tokens-Text-text-primary, #222);
+  font-family: 'Roboto Mono';
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 40px; /* 125% */
+`;
+const generateTableRows = (data: InputOutputTable) => {
+  const header = data[0]
+    ? Object.keys(data[0]).map((key) => capitalize(key))
+    : [];
+  const headerRow = {
+    cells: header,
+    _key: 'th',
+  };
+  const rows = data.map((row, i) => {
+    return {
+      cells: [...Object.values(row).map((property) => `${property}`)],
+      _key: `tr${i}${data.length}`,
+    };
+  });
+  return { rows: [headerRow, ...rows] };
+};
+
+export default function EndpointBlock(props: { endpoint: EndpointInfo }) {
+  const {
+    title,
+    description,
+    params,
+    output,
+    sampleResponse,
+    path,
+    requestType,
+    paramTypes,
+  } = props.endpoint;
+  return (
+    <OakFlex $gap="all-spacing-3" $flexDirection="column" $mb="space-between-l">
+      <OakHeading tag="h2" $font="heading-4">
+        {title}
+      </OakHeading>
+      <EndpointHeading
+        className={robotoMono.className}
+        tag="h2"
+        $font="heading-5"
+      >
+        {requestType.toUpperCase()} {path}
+      </EndpointHeading>
+      {description && <OakP>{description}</OakP>}
+      <OakHeading tag="h3" $font="heading-5">
+        Inputs {paramTypes.length ? `(${paramTypes.join(', ')})` : ''}
+      </OakHeading>
+      <OakBox>
+        {params && params.length > 1 ? (
+          <Table value={generateTableRows(params)} />
+        ) : (
+          'N/ A'
+        )}
+      </OakBox>
+      <OakHeading tag="h3" $font="heading-5">
+        Output (response)
+        {output && output[0] ? (
+          <Table value={generateTableRows(output)} />
+        ) : (
+          <OakP>N/ A</OakP>
+        )}
+      </OakHeading>
+      {sampleResponse && (
+        <OakBox>
+          <OakHeading tag="h3" $font="heading-5">
+            Sample response
+          </OakHeading>
+          <Code value={{ code: sampleResponse, language: 'json' }} />
+        </OakBox>
+      )}
+    </OakFlex>
+  );
+}
