304fb736ea097865cf8fa982e9acaf2432dcd82d
Sonali Singh <sonali.singh@live.co.uk>
2025-08-12 14:32:34 +0100

    refactor: remove page generation for endpoints

--- src/cms/queries/navDocsListQuery/navDocsListQuery.query.ts
+++ src/cms/queries/navDocsListQuery/navDocsListQuery.query.ts
@@ -6,20 +6,8 @@ import {
   NavGroup,
 } from '@/cms/schemaTypes/curriculumApiDocsNav.schema';
 import query from './navDocsListQuery.gql';
-import { groupedEndpointInfo } from '@/lib/endpoint-docs/getEndpointDocs';
 
 const API_ENDPOINTS_SLUG = 'api-endpoints';
-
-const getEndpointsNavData = () => {
-  return Object.values(groupedEndpointInfo).map((page, order) => {
-    const { title, slug }: { title: string; slug: string } = page;
-
-    return {
-      title: `${order + 1}. ${title}`,
-      href: `${API_ENDPOINTS_SLUG}/${slug}`,
-    };
-  });
-};
 
 const navDocsListQuery = async () => {
   const res = await client.request(query);
@@ -41,14 +29,14 @@ const navDocsListQuery = async () => {
       const groupSlug = group.slug.text;
       const pages = input.pages
         .filter((page) => page.parentGroup?.slug?.text === groupSlug)
-        .map((page) => ({
-          title: page.title,
+        .map((page, order) => ({
+          title:
+            // don't add a number to the overview page. Query already orders the pages
+            groupSlug === API_ENDPOINTS_SLUG && !page.title.includes('overview')
+              ? `${order}. ${page.title}`
+              : page.title,
           href: `${groupSlug}/${page.slug.text}`,
         }));
-
-      if (groupSlug === API_ENDPOINTS_SLUG) {
-        pages.push(...getEndpointsNavData());
-      }
 
       return {
         title: group.title,
