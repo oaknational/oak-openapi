d069f8b5ef2c540c03e8e71aee301ef5bfc81b07
Sonali Singh <sonali.singh@live.co.uk>
2025-08-13 13:23:37 +0100

    feat: added descriptions for sequences

--- src/lib/handlers/assets/schemas/requests/sequenceAssetsRequest.schema.ts
+++ src/lib/handlers/assets/schemas/requests/sequenceAssetsRequest.schema.ts
@@ -1,8 +1,19 @@
 import z from 'zod';
 import { downloadTypeEnum } from '@/lib/handlers/assets/types';
+import 'zod-openapi/extend';
 
 export const sequenceAssetsRequestSchema = z.object({
-  sequence: z.string(),
-  year: z.number().optional(),
-  type: downloadTypeEnum.optional(),
+  sequence: z.string().openapi({
+    description:
+      'The sequence slug identifier, including the key stage 4 option where relevant.',
+  }),
+  year: z.number().optional().openapi({
+    description:
+      'The year group to filter by. For the physical-education-primary sequence, a value of all-years can also be used.',
+  }),
+  type: downloadTypeEnum.optional().openapi({
+    description: `Optional asset type specifier
+
+Available values: slideDeck, exitQuiz, exitQuizAnswers, starterQuiz, starterQuizAnswers, supplementaryResource, video, worksheet, worksheetAnswers`,
+  }),
 });
--- src/lib/zod-openapi/generated/assets/sequenceAssetsRequest.openapi.ts
+++ src/lib/zod-openapi/generated/assets/sequenceAssetsRequest.openapi.ts
@@ -1,11 +1,23 @@
-import 'zod-openapi/extend';
 import z from 'zod';
 import { downloadTypeEnum } from '@/lib/handlers/assets/types';
+import 'zod-openapi/extend';
 
 export const sequenceAssetsRequestOpenAPISchema = z
   .object({
-    sequence: z.string().openapi({ example: 'maths-secondary' }),
-    year: z.number().optional(),
-    type: downloadTypeEnum.optional(),
+    sequence: z.string().openapi({
+      description:
+        'The sequence slug identifier, including the key stage 4 option where relevant.',
+    }),
+    year: z.number().optional().openapi({
+      description:
+        'The year group to filter by. For the physical-education-primary sequence, a value of all-years can also be used.',
+    }),
+    type: downloadTypeEnum.optional().openapi({
+      description: `Optional asset type specifier
+
+Available values: slideDeck, exitQuiz, exitQuizAnswers, starterQuiz, starterQuizAnswers, supplementaryResource, video, worksheet, worksheetAnswers`,
+    }),
   })
-  .openapi({ example: { sequence: 'maths-secondary' } });
+  .openapi({
+    example: { sequence: 'maths-primary', year: '3', type: 'video' },
+  });
