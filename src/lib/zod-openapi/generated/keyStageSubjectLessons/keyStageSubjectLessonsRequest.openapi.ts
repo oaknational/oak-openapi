12d2063c8a8a42f18fa4dc39e53d5dddcd698fe7
Sonali Singh <sonali.singh@live.co.uk>
2025-08-13 22:39:32 +0100

    fix: refactor unused code

--- src/lib/zod-openapi/generated/keyStageSubjectLessons/keyStageSubjectLessonsRequest.openapi.ts
+++ src/lib/zod-openapi/generated/keyStageSubjectLessons/keyStageSubjectLessonsRequest.openapi.ts
@@ -3,14 +3,20 @@ import { keyStageSlugs, subjectSlugs } from '@/lib/keyStageAndSubjects';
 import z from 'zod';
 
 export const keyStageSubjectLessonsRequestOpenAPISchema = z.object({
-  keyStage: z.enum(keyStageSlugs as [string], {
-    description:
-      "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
-  }),
-  subject: z.enum(subjectSlugs as [string], {
-    description:
-      "Subject slug to filter by, e.g. 'english' - note that casing is important here, and should be lowercase",
-  }),
+  keyStage: z
+    .enum(keyStageSlugs as [string])
+    .openapi({
+      description:
+        "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
+      example: 'ks1',
+    }),
+  subject: z
+    .enum(subjectSlugs as [string])
+    .openapi({
+      description:
+        "Subject slug to filter by, e.g. 'english' - note that casing is important here, and should be lowercase",
+      example: 'english',
+    }),
   unit: z
     .string({
       description: 'Optional unit slug to additionally filter by',
--- src/lib/zod-openapi/generated/questions/questionsForKeyStageAndSubjectRequest.openapi.ts
+++ src/lib/zod-openapi/generated/questions/questionsForKeyStageAndSubjectRequest.openapi.ts
@@ -3,14 +3,20 @@ import { keyStageSlugs, subjectSlugs } from '@/lib/keyStageAndSubjects';
 import z from 'zod';
 
 export const questionsForKeyStageAndSubjectRequestOpenAPISchema = z.object({
-  keyStage: z.enum(keyStageSlugs as [string], {
-    description:
-      "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
-  }),
-  subject: z.enum(subjectSlugs as [string], {
-    description:
-      "Subject slug to search by, e.g. 'science' - note that casing is important here",
-  }),
+  keyStage: z
+    .enum(keyStageSlugs as [string])
+    .openapi({
+      description:
+        "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
+      example: 'ks1',
+    }),
+  subject: z
+    .enum(subjectSlugs as [string])
+    .openapi({
+      description:
+        "Subject slug to search by, e.g. 'science' - note that casing is important here",
+      example: 'art',
+    }),
   offset: z
     .number({
       description:
