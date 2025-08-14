ecde28117cf0b9bdacdb0c3cf98c7edb51fe5d54
Sonali Singh <sonali.singh@live.co.uk>
2025-08-14 11:33:28 +0100

    feat: generated files with new descriptions

--- src/lib/zod-openapi/generated/keyStageSubjectLessons/keyStageSubjectLessonsRequest.openapi.ts
+++ src/lib/zod-openapi/generated/keyStageSubjectLessons/keyStageSubjectLessonsRequest.openapi.ts
@@ -1,19 +1,23 @@
 import 'zod-openapi/extend';
 import { keyStageSlugs, subjectSlugs } from '@/lib/keyStageAndSubjects';
-import z from 'zod';
 import { limitSchema, offsetSchema } from '@/lib/handlers/commonTypes';
+import z from 'zod';
 
 export const keyStageSubjectLessonsRequestOpenAPISchema = z.object({
-  keyStage: z.enum(keyStageSlugs as [string]).openapi({
-    description:
-      "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
-    example: 'ks1',
-  }),
-  subject: z.enum(subjectSlugs as [string]).openapi({
-    description:
-      "Subject slug to filter by, e.g. 'english' - note that casing is important here, and should be lowercase",
-    example: 'english',
-  }),
+  keyStage: z
+    .enum(keyStageSlugs as [string])
+    .openapi({
+      description:
+        "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
+      example: 'ks1',
+    }),
+  subject: z
+    .enum(subjectSlugs as [string])
+    .openapi({
+      description:
+        "Subject slug to filter by, e.g. 'english' - note that casing is important here, and should be lowercase",
+      example: 'english',
+    }),
   unit: z
     .string()
     .openapi({
