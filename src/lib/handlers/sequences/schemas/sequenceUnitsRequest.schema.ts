7aae1b527bb1a4a766bf0edf5ff551f5bd4bbcc7
Remy Sharp <remy@remysharp.com>
2025-08-14 12:40:55 +0100

    chore: reuse yearSequenceSchema

--- src/lib/handlers/sequences/schemas/sequenceUnitsRequest.schema.ts
+++ src/lib/handlers/sequences/schemas/sequenceUnitsRequest.schema.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod';
-import { sequenceSlugSchema, years } from '@/lib/handlers/sequences/types';
+import {
+  sequenceSlugSchema,
+  sequenceYearSchema,
+} from '@/lib/handlers/sequences/types';
 import 'zod-openapi/extend';
 
 export const sequenceUnitsRequestSchema = z.object({
   sequence: sequenceSlugSchema,
-  year: z
-    .enum(years as [string], {
-      description:
-        'The year group to filter by. For the physical-education-primary sequence, a value of all-years can also be used.',
-    })
-    .optional(),
+  year: sequenceYearSchema,
 });
