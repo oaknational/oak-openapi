40e09d6db47abff49b82600b037f71995341b47b
Remy Sharp <remy@remysharp.com>
2025-08-14 14:57:34 +0100

    fix: update thread examples

--- src/lib/zod-openapi/generated/threads/threadUnitsRequest.openapi.ts
+++ src/lib/zod-openapi/generated/threads/threadUnitsRequest.openapi.ts
@@ -2,10 +2,8 @@ import 'zod-openapi/extend';
 import { z } from 'zod';
 
 export const threadUnitsRequestOpenAPISchema = z.object({
-  threadSlug: z
-    .string()
-    .openapi({
-      description: 'The thread identifier for a given unit',
-      example: 'a-midsummer-nights-dream-72',
-    }),
+  threadSlug: z.string().openapi({
+    description: 'The thread identifier for a given unit',
+    example: 'number-multiplication-and-division',
+  }),
 });
