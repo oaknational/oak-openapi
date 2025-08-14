22ee5ee23e79ce3af1c4004fb3a1a2eaacb64e5b
Remy Sharp <remy@remysharp.com>
2025-08-14 12:48:35 +0100

    fix: use the year enum instead

--- src/lib/handlers/sequences/schemas/sequenceUnitsRequest.schema.ts
+++ src/lib/handlers/sequences/schemas/sequenceUnitsRequest.schema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 import {
   sequenceSlugSchema,
-  sequenceYearSchema,
+  sequenceYearEnumSchema,
 } from '@/lib/handlers/sequences/types';
 import 'zod-openapi/extend';
 
 export const sequenceUnitsRequestSchema = z.object({
   sequence: sequenceSlugSchema,
-  year: sequenceYearSchema,
+  year: sequenceYearEnumSchema,
 });
--- src/lib/zod-openapi/generated/sequences/sequenceUnitsRequest.openapi.ts
+++ src/lib/zod-openapi/generated/sequences/sequenceUnitsRequest.openapi.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 import {
   sequenceSlugSchema,
-  sequenceYearSchema,
+  sequenceYearEnumSchema,
 } from '@/lib/handlers/sequences/types';
 import 'zod-openapi/extend';
 
 export const sequenceUnitsRequestOpenAPISchema = z.object({
   sequence: sequenceSlugSchema,
-  year: sequenceYearSchema,
+  year: sequenceYearEnumSchema,
 });
