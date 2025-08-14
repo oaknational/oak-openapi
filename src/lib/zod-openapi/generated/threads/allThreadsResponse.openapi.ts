d24e46d0462ce82186137fc1b4cd8b4b7a4a64a4
Remy Sharp <remy@remysharp.com>
2025-08-14 14:57:34 +0100

    fix: update thread examples

--- src/lib/zod-openapi/generated/threads/allThreadsResponse.openapi.ts
+++ src/lib/zod-openapi/generated/threads/allThreadsResponse.openapi.ts
@@ -2,14 +2,16 @@ import 'zod-openapi/extend';
 import { z } from 'zod';
 import { threadSchema } from '@/lib/handlers/threads/types';
 
-export const allThreadsResponseOpenAPISchema = z
-  .array(threadSchema)
-  .openapi({
-    ref: 'AllThreadsResponseSchema',
-    example: [
-      {
-        title: 'A Midsummer Night\u2019s Dream',
-        slug: 'a-midsummer-nights-dream-72',
-      },
-    ],
-  });
+export const allThreadsResponseOpenAPISchema = z.array(threadSchema).openapi({
+  ref: 'AllThreadsResponseSchema',
+  example: [
+    {
+      title: 'Number: Multiplication and division',
+      slug: 'number-multiplication-and-division',
+    },
+    {
+      title: 'Number: Place value',
+      slug: 'number-place-value',
+    },
+  ],
+});
