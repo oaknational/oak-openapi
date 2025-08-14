b236d03dd2e0e13015ea4c4782f54aa49c3a9b22
Sonali Singh <sonali.singh@live.co.uk>
2025-08-14 10:37:28 +0100

    feat: add lesson asset type

--- src/lib/zod-openapi/generated/assets/lessonAssetResponse.openapi.ts
+++ src/lib/zod-openapi/generated/assets/lessonAssetResponse.openapi.ts
@@ -2,7 +2,11 @@ import 'zod-openapi/extend';
 import { z } from 'zod';
 
 export const lessonAssetResponseOpenAPISchema = z
-  .any()
+  .record(
+    z.string({ description: 'The HTTP status code' }),
+    z.string({ description: 'The content type of the download' }),
+  )
+  .optional()
   .openapi({
     ref: 'LessonAssetResponseSchema',
     example: { '200': 'application/octet-stream' },
