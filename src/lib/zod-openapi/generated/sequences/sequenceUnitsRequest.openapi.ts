80db33e875f382dba467df088133a7b5ceee73fb
Sonali Singh <sonali.singh@live.co.uk>
2025-08-14 12:04:58 +0100

    fix: revert units in sequence request to accept enum

--- src/lib/zod-openapi/generated/sequences/sequenceUnitsRequest.openapi.ts
+++ src/lib/zod-openapi/generated/sequences/sequenceUnitsRequest.openapi.ts
@@ -1,11 +1,15 @@
 import { z } from 'zod';
-import {
-  sequenceSlugSchema,
-  sequenceYearSchema,
-} from '@/lib/handlers/sequences/types';
+import { sequenceSlugSchema, years } from '@/lib/handlers/sequences/types';
 import 'zod-openapi/extend';
 
 export const sequenceUnitsRequestOpenAPISchema = z.object({
   sequence: sequenceSlugSchema,
-  year: sequenceYearSchema.optional(),
+  year: z
+    .enum(years as [string])
+    .openapi({
+      description:
+        'The year group to filter by. For the physical-education-primary sequence, a value of all-years can also be used.',
+      example: '1',
+    })
+    .optional(),
 });
