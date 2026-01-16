a959b840f5a7abcf9cf14ba64d34a90102928432
Remy Sharp <remy@remysharp.com>
2026-01-16 17:38:15 +0000

    chore: fix the last of the eslint errors
    I hope…

--- src/lib/handlers/keyStageSubjectLessons/schemas/keyStageSubjectLessonsRequest.schema.ts
+++ src/lib/handlers/keyStageSubjectLessons/schemas/keyStageSubjectLessonsRequest.schema.ts
@@ -3,11 +3,11 @@ import { limitSchema, offsetSchema } from '@/lib/handlers/commonTypes';
 import * as z from 'zod/v4';
 
 export const keyStageSubjectLessonsRequestSchema = z.object({
-  keyStage: z.enum(keyStageSlugs as [string], {
+  keyStage: z.enum(keyStageSlugs as [string]).meta({
     description:
       "Key stage slug to filter by, e.g. 'ks2' - note that casing is important here, and should be lowercase",
   }),
-  subject: z.enum(subjectSlugs as [string], {
+  subject: z.enum(subjectSlugs as [string]).meta({
     description:
       "Subject slug to filter by, e.g. 'english' - note that casing is important here, and should be lowercase",
   }),
--- src/lib/handlers/keyStageSubjectLessons/schemas/keyStageSubjectLessonsResponse.schema.ts
+++ src/lib/handlers/keyStageSubjectLessons/schemas/keyStageSubjectLessonsResponse.schema.ts
@@ -4,12 +4,13 @@ export const keyStageSubjectLessonsResponseSchema = z.array(
   z.object({
     unitSlug: z.string().describe('The unit slug identifier'),
     unitTitle: z.string().describe('The unit title'),
-    lessons: z.array(
-      z.object({
-        lessonSlug: z.string().describe('The lesson slug identifier'),
-        lessonTitle: z.string().describe('The lesson title'),
-      }),
-      { description: 'List of lessons for the specified unit' },
-    ),
+    lessons: z
+      .array(
+        z.object({
+          lessonSlug: z.string().describe('The lesson slug identifier'),
+          lessonTitle: z.string().describe('The lesson title'),
+        }),
+      )
+      .meta({ description: 'List of lessons for the specified unit' }),
   }),
 );
