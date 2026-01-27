0c6255b3c6089dbae45ce6f476ac3f633b784b7d
Kofoworola Ogunleye <kay16th@yahoo.com>
2026-01-27 07:52:55 -0500

    Revert "feat: set up in vercel"
    This reverts commit af940faa51ecb42df802d5ebd3f267dd88e4487f.

--- infrastructure/project/main.tf
+++ infrastructure/project/main.tf
@@ -1,9 +1,5 @@
 locals {
   workspace_prefix = "oak-openapi-project-"
-
-  build_type = replace(terraform.workspace, local.workspace_prefix, "")
-
-  build_config = local.builds[local.build_type]
 }
 
 resource "terraform_data" "workspace_validation" {
@@ -16,16 +12,16 @@ resource "terraform_data" "workspace_validation" {
 }
 
 module "vercel" {
-  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.3.7"
-  build_command                    = try(local.build_config.build_command, null)
-  build_type                       = local.build_config.build_type
+  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.3.6"
+  build_command                    = "pnpm build"
+  build_type                       = "website"
   cloudflare_zone_domain           = var.cloudflare_zone_domain
-  domains                          = try(local.build_config.domains, [])
-  environment_variables            = try(local.build_config.environment_variables, [])
-  framework                        = local.build_config.framework
+  environment_variables            = local.environment_variables
+  framework                        = "nextjs"
+  project_visibility               = "public"
   git_repo                         = "oaknational/oak-openapi"
   install_command                  = "pnpm install"
-  project_visibility               = "public"
   protection_bypass_for_automation = false
-  skew_protection                  = try(local.build_config.skew_protection, null)
+
+  domains = ["open-api.thenational.academy"]
 }
