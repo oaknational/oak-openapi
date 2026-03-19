c3fb9c79c3b8bca9569533f317511fc145897e3e
Remy Sharp <remy@remysharp.com>
2026-03-19 15:43:50 +0000

    fix: tests and schema for keywords

--- src/lib/handlers/keywords/schemas/keywordsResponse.schema.ts
+++ src/lib/handlers/keywords/schemas/keywordsResponse.schema.ts
@@ -4,6 +4,12 @@ export const keywordsResponseSchema = z.array(
   z.object({
     keyword: z.string().describe('The keyword text'),
     description: z.string().describe('A description of the keyword'),
+    keyStageSlug: z
+      .string()
+      .describe('The key stage slug associated with the keyword'),
+    subjectSlug: z
+      .string()
+      .describe('The subject slug associated with the keyword'),
     lessonSlugs: z
       .array(z.string())
       .describe('The different lesson slugs where this keyword is used'),
