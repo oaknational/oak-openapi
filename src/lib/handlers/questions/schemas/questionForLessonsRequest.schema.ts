8bbb62dd81fc8a0f80cf6bdbf76f8dc81293d4c2
Remy Sharp <remy@remysharp.com>
2026-05-06 12:59:56 +0100

    chore: WIP adding filter for question + images

--- src/lib/handlers/questions/schemas/questionForLessonsRequest.schema.ts
+++ src/lib/handlers/questions/schemas/questionForLessonsRequest.schema.ts
@@ -1,5 +1,7 @@
 import * as z from 'zod/v4';
+import { questionFilterSchema } from '../types';
 
 export const questionForLessonsRequestSchema = z.object({
   lesson: z.string().describe('The lesson slug identifier'),
+  filter: questionFilterSchema.optional(),
 });
